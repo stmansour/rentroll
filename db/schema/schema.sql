11c32e47bf85ad0c61b9b7b78f57463b98009161
Steve Mansour <sman@stevemansour.com>
2016-01-18 15:10:36 -0800

    wip

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -1,6 +1,11 @@
--- *************************
--- *********  IDS  *********
--- *************************
+-- Conventions used:
+--     Table names are all lower case
+--     Field names are camel case
+--     Money values are all stored as DECIMAL(19,4)
+
+-- ********************************
+-- *********  UNIQUE IDS  *********
+-- ********************************
 --    PRID = property id
 --    UTID = unit type id
 --   USPID = unit specialty id
@@ -11,7 +16,10 @@
 --  BLDGID = building id
 --  UNITID = unit id
 --    TCID = transactant id
-
+--     TID = tenant id
+--     PID = payor id
+--   OAMID = occupancy agreement master id
+--   OASID = occupancy agreement subpart
 
 -- **************************************
 -- ****                              ****
@@ -33,8 +41,8 @@ CREATE TABLE property (
     OccupancyType SMALLINT NOT NULL DEFAULT 0,
     ParkingPermitInUse SMALLINT NOT NULL DEFAULT 0,
     MOAFID MEDIUMINT NOT NULL DEFAULT 0,
-    LastModTime TIMESTAMP,
-    LastModBy MEDIUMINT NOT NULL DEFAULT 0,
+    LastModTime TIMESTAMP,                                  -- when was this record last written
+    LastModBy MEDIUMINT NOT NULL DEFAULT 0,                 -- employee UID (from phonebook) that modified it 
     PRIMARY KEY (PRID)
 );
 
@@ -43,6 +51,8 @@ CREATE TABLE unittypes (
     PRID MEDIUMINT NOT NULL,                            -- associated property id
     Designation CHAR(5) NOT NULL DEFAULT '',
     Description VARCHAR(256) NOT NULL DEFAULT '',
+    LastModTime TIMESTAMP,                                  -- when was this record last written
+    LastModBy MEDIUMINT NOT NULL DEFAULT 0,                 -- employee UID (from phonebook) that modified it 
     PRIMARY KEY (UTID)
 );
 
@@ -55,7 +65,6 @@ CREATE TABLE unitspecialties (
     Fee DECIMAL(19,4) NOT NULL DEFAULT 0.0,
     Description VARCHAR(256) NOT NULL DEFAULT '',
     PRIMARY KEY (USPID)
-
 );
 
 CREATE TABLE offsets (
@@ -63,15 +72,27 @@ CREATE TABLE offsets (
     Description VARCHAR(256) NOT NULL DEFAULT ''
 );
 
+-- This describes the universe of possible assessments
 CREATE TABLE assessments (
-    ASMID MEDIUMINT NOT NULL,
-    Description VARCHAR(256) NOT NULL DEFAULT ''   
+    ASMID MEDIUMINT NOT NULL AUTO_INCREMENT,
+    Description VARCHAR(256) NOT NULL DEFAULT '',
+    PRIMARY KEY(ASMID)
+);
+
+-- This describes the world of assessments for a particular property
+-- query this table based on the PRID, the solution set is the list
+-- of assessments for that particular property.
+CREATE TABLE propertyassessments (
+    PRID MEDIUMINT NOT NULL DEFAULT 0,
+    ASMID MEDIUMINT NOT NULL DEFAULT 0,
 );
 
 CREATE TABLE paymenttypes (
     PMTID MEDIUMINT NOT NULL AUTO_INCREMENT,
     Description VARCHAR(256) NOT NULL DEFAULT ''
     PRIMARY KEY (PMTID)
+    LastModTime TIMESTAMP,                                  -- when was this record last written
+    LastModBy MEDIUMINT NOT NULL DEFAULT 0,                 -- employee UID (from phonebook) that modified it 
 );
 
 
@@ -99,6 +120,9 @@ CREATE TABLE buildings (
     State CHAR(25) NOT NULL DEFAULT '',
     PostalCode VARCHAR(10) NOT NULL DEFAULT '',
     Country VARCHAR(25) NOT NULL DEFAULT ''
+    LastModTime TIMESTAMP,                                  -- when was this record last written
+    LastModBy MEDIUMINT NOT NULL DEFAULT 0,                 -- employee UID (from phonebook) that modified it 
+    PRIMARY KEY (BLDGID)
 );
 
 -- **************************************
@@ -116,14 +140,18 @@ CREATE TABLE units (
     ScheduledRent DECIMAL(19,4) NOT NULL DEFAULT 0.0,   -- budgeted rent for this unit
     Assignment SMALLINT NOT NULL DEFAULT 0,             -- Pre-assign or assign at occupy commencement
     Report SMALLINT NOT NULL DEFAULT 1,                 -- 1 = apply to rentroll, 0 = skip on rentroll
+    LastModTime TIMESTAMP,                                  -- when was this record last written
+    LastModBy MEDIUMINT NOT NULL DEFAULT 0,                 -- employee UID (from phonebook) that modified it 
+    PRIMARY KEY (UNITID)
 );
 
 -- For each unit, what specialties does it have...
--- this is simply a list of USPIDs.  So selecting all entries where the unit == UNITID
--- will be the list of all the unit specialties.
+-- this is simply a list of USPIDs.
+-- Selecting all entries where the unit == UNITID
+-- will be the list of all the unit specialties for that unit.
 CREATE TABLE unitspecialties (
     UNITID MEDIUMINT NOT NULL,                          -- unique id of unit
-    USPID MEDIUMINT NOT NULL,                           -- unique id of specialty
+    USPID MEDIUMINT NOT NULL,                           -- unique id of specialty (see Table unitspecialties)
 );
 
 -- **************************************
@@ -132,58 +160,123 @@ CREATE TABLE unitspecialties (
 -- ****                              ****
 -- **************************************
 
--- transactant - fields common to all people
+-- transactant - fields common to all people and
+-- ids of prospect/tenant/payor as appropriate
 CREATE TABLE transactant (
     TCID MEDIUMINT NOT NULL AUTO_INCREMENT,              -- unique id of unit
+    TID MEDIUMINT NOT NULL DEFAULT 0,                   -- associated tenant id
+    PID MEDIUMINT NOT NULL DEFAULT 0,                   -- associated payor id
     FirstName VARCHAR(35) NOT NULL DEFAULT '',
     MiddleName VARCHAR(35) NOT NULL DEFAULT '',
     LastName VARCHAR(35) NOT NULL DEFAULT '', 
+    PrimaryEmail VARCHAR(35) NOT NULL DEFAULT '',
+    SecondaryEmail VARCHAR(35) NOT NULL DEFAULT '',
+    WorkPhone VARCHAR(25) NOT NULL DEFAULT '',
+    CellPhone VARCHAR(25) NOT NULL DEFAULT '',
     Address VARCHAR(35) NOT NULL DEFAULT '',            -- person address
     Address2 VARCHAR(35) NOT NULL DEFAULT '',       
     City VARCHAR(25) NOT NULL DEFAULT '',
     State CHAR(25) NOT NULL DEFAULT '',
     PostalCode VARCHAR(10) NOT NULL DEFAULT '',
     Country VARCHAR(25) NOT NULL DEFAULT ''
-    LastModTime TIMESTAMP,
-    LastModBy MEDIUMINT NOT NULL DEFAULT 0,
+    LastModTime TIMESTAMP,                                  -- when was this record last written
+    LastModBy MEDIUMINT NOT NULL DEFAULT 0,                 -- employee UID (from phonebook) that modified it 
     PRIMARY KEY (TCID)
 );
 
 CREATE TABLE prospects (
+    TID MEDIUMINT NOT NULL AUTO_INCREMENT,                  -- unique id of this tenant
+    TCID MEDIUMINT NOT NULL,                                -- associated transactant (has Name and all contact info)
+    LastModTime TIMESTAMP,                                  -- when was this record last written
+    LastModBy MEDIUMINT NOT NULL DEFAULT 0,                 -- employee UID (from phonebook) that modified it 
 );
 
 CREATE TABLE tenant (
-    TID MEDIUMINT NOT NULL AUTO_INCREMENT,              -- unique id of this tenant
-    TCID MEDIUMINT NOT NULL,                             -- associated transactant
+    TID MEDIUMINT NOT NULL AUTO_INCREMENT,                      -- unique id of this tenant
+    TCID MEDIUMINT NOT NULL,                                    -- associated transactant
+    CarMake VARCHAR(25) NOT NULL DEFAULT '',
+    CarModel VARCHAR(25) NOT NULL DEFAULT '',
     CarColor VARCHAR(25) NOT NULL DEFAULT '',
     CarYear MEDIUMINT NOT NULL DEFAULT 0,
-    AccountRep VARCHAR(35) NOT NULL DEFAULT '',
     LicensePlateState VARCHAR(35) NOT NULL DEFAULT '',
     LicensePlateNumber VARCHAR(35) NOT NULL DEFAULT '',
     ParkingPermitNumber VARCHAR(35) NOT NULL DEFAULT '',
+    AccountRep MEDIUMINT NOT NULL DEFAULT 0,                    -- Phonebook UID of account rep
     DateofBirth DATE NOT NULL DEFAULT '2000-01-01 00:00:00',
     EmergencyContactName VARCHAR(35) NOT NULL DEFAULT '',
     EmergencyContactAddress VARCHAR(35) NOT NULL DEFAULT '',
-    EmergencyContactTelephone VARCHAR(35) NOT NULL DEFAULT '',
+    EmergencyContactTelephone VARCHAR(25) NOT NULL DEFAULT '',
     EmergencyAddressEmail VARCHAR(35) NOT NULL DEFAULT '',
     AlternateAddress VARCHAR(35) NOT NULL DEFAULT '',
-    ElibigleForFutureOccupancy SMALLINT NOT NULL DEFAULT 1,
-    Industry VARCHAR(35) NOT NULL DEFAULT '',
-    Source  VARCHAR(35) NOT NULL DEFAULT '',
-    InvoicingCustomerNumber VARCHAR(35) NOT NULL DEFAULT ''
-    LastModTime TIMESTAMP,
-    LastModBy MEDIUMINT NOT NULL DEFAULT 0,
+    ElibigleForFutureOccupancy SMALLINT NOT NULL DEFAULT 1,         -- yes/no
+    Industry VARCHAR(35) NOT NULL DEFAULT '',                       -- (e.g., construction, retail, banking etc.)
+    Source  VARCHAR(35) NOT NULL DEFAULT '',                        -- (e.g., resident referral, newspaper, radio, post card, expedia, travelocity, etc.)
+    InvoicingCustomerNumber VARCHAR(35) NOT NULL DEFAULT ''         -- [drawn from the invoicing section] [only applies if invoicing authorization has been provide
+    LastModTime TIMESTAMP,                                          -- when was this record last written
+    LastModBy MEDIUMINT NOT NULL DEFAULT 0,                         -- employee UID (from phonebook) that modified it 
     PRIMARY KEY (TID)
 );
 
-
-
-
-
 CREATE TABLE payor  (
+    PID MEDIUMINT NOT NULL AUTO_INCREMENT,                          -- unique id of this payor
+    TCID MEDIUMINT NOT NULL,                                        -- associated transactant
+    CreditLimit DECIMAL(19,4) NOT NULL DEFAULT 0.0,
+    --
+    --  JOE: there must be more credit information???
+    --
+    EmployerName  VARCHAR(35) NOT NULL DEFAULT '',
+    EmployerStreetAddress VARCHAR(35) NOT NULL DEFAULT '',
+    EmployerCity VARCHAR(35) NOT NULL DEFAULT '',
+    EmployerState VARCHAR(35) NOT NULL DEFAULT '',
+    EmployerZipcode VARCHAR(35) NOT NULL DEFAULT '',
+    Occupation VARCHAR(35) NOT NULL DEFAULT '',
+    LastModTime TIMESTAMP,                                          -- when was this record last written
+    LastModBy MEDIUMINT NOT NULL DEFAULT 0,                         -- employee UID (from phonebook) that modified it 
+    PRIMARY KEY (PID)
 );
 
+-- **************************************
+-- ****                              ****
+-- ****      OCCUPANCY AGREEMENT     ****
+-- ****                              ****
+-- **************************************
 
+CREATE TABLE occupancyagreementmaster (
+    OAMID MEDIUMINT NOT NULL AUTO_INCREMENT,                        -- internal unique id
+    ReferenceNumber VARCHAR(35) DEFAULT '',                         -- Occupancy Agreement Reference Number
+    RentalAgreementType SMALLINT NOT NULL DEFAULT 0,                -- leasehold, month-to-month, hotel
+    PID MEDIUMINT NOT NULL DEFAULT 0,                               -- who is the payor for this agreement
+    LastModTime TIMESTAMP,                                          -- when was this record last written
+    LastModBy MEDIUMINT NOT NULL DEFAULT 0,                         -- employee UID (from phonebook) that modified it 
+    PRIMARY KEY (OAMID)     
+);      
+        
+CREATE TABLE occupancyagreementsubpart (        
+    OASID MEDIUMINT NOT NULL AUTO_INCREMENT,                        -- internal unique id
+    OAMID MEDIUMINT NOT NULL DEFAULT 0,                             -- reference to Occupancy Master Agreement
+    UNITID MEDIUMINT NOT NULL DEFAULT 0,                            -- associated unit
+    PrimaryTenant MEDIUMINT NOT NULL DEFAULT 0,                     -- TID of primary tenant.  
+    OccupancyStart DATE NOT NULL DEFAULT '2000-01-01 00:00:00',
+    OccupancyEnd DATE NOT NULL DEFAULT '2000-01-01 00:00:00',
+    Renewal SMALLINT NOT NULL DEFAULT 0,                            -- month to month automatic renewal, lease extension options, none.
+    ProrationMethod SMALLINT NOT NULL DEFAULT 0,                    -- daily, monthly based on actual, monthly based on 30 days
+    SpecialProvisions VARCHAR(50) NOT NULL DEFAULT '',  -- JOE: what are these?  arbitrary strings? Multi-value? do we need a separate table?
+);
+
+-- query this table for rows where OASID=(the occupancy agreement for the unit)
+-- the return list will be the TIDs of all tenants in that unit
+CREATE TABLE unittenants (
+    OASID MEDIUMINT NOT NULL DEFAULT 0,         -- the unit's occupancy agreement
+    TID MEDIUMINT NOT NULL DEFAULT 0            -- the tenant
+);
+
+-- for a specific unit, what are the recurring assessments
+CREATE TABLE unitrecurringassessments (
+    UNITID MEDIUMINT NOT NULL DEFAULT 0,        -- unit associated with this assessment
+    ASMID MEDIUMINT NOT NULL DEFAULT 0,         -- what type of assessment
+    Amount DECIMAL(19,4) NOT NULL DEFAULT 0.0,   -- assessment amount
+    
+);
 
 -- Add the Administrator as the first and only user
 -- INSERT INTO people (UserName,FirstName,LastName) VALUES("administrator","Administrator","Administrator");
