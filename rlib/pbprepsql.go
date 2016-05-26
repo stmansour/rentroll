f286bf649392dc2a05c013e034c6ba6130925807
Steve Mansour <sman@stevemansour.com>
2016-05-26 10:26:39 -0700

    forgot to add pbprepsql.go

---
+++ rlib/pbprepsql.go
@@ -0,0 +1,20 @@
+package rlib
+
+func buildPBPreparedStatements() {
+	var err error
+
+	//==========================================
+	// BUSINESS UNIT
+	//==========================================
+	RRdb.PBsql.GetBusinessUnitByDesignation, err = RRdb.dbdir.Prepare("SELECT ClassCode,CoCode,Name,Designation,Description,LastModTime,LastModBy FROM classes WHERE Designation=?")
+	Errcheck(err)
+
+	//==========================================
+	// COMPANY
+	//==========================================
+	RRdb.PBsql.GetCompany, err = RRdb.dbdir.Prepare("SELECT CoCode,LegalName,CommonName,Address,Address2,City,State,PostalCode,Country,Phone,Fax,Email,Designation,Active,EmploysPersonnel,LastModTime,LastModBy FROM companies WHERE CoCode=?")
+	Errcheck(err)
+	RRdb.PBsql.GetCompanyByDesignation, err = RRdb.dbdir.Prepare("SELECT CoCode,LegalName,CommonName,Address,Address2,City,State,PostalCode,Country,Phone,Fax,Email,Designation,Active,EmploysPersonnel,LastModTime,LastModBy FROM companies WHERE Designation=?")
+	Errcheck(err)
+
+}
