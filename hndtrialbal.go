6586683e45c731c2277c2d2ae8c439e316a411b2
Steve Mansour <sman@stevemansour.com>
2016-06-22 23:06:47 -0700

    Ledger Balance working in UI

--- hndtrialbal.go
+++ hndtrialbal.go
@@ -30,14 +30,23 @@ func hndTrialBalance(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("%s: Invalid start date:  %s\n", funcname, D2)
 	}
 
+	fmt.Printf("Will look for biz\n")
 	if len(des) > 0 {
-		biz, _ := rlib.GetBusinessByDesignation(des)
-		if len(biz.Designation) == 0 {
-			rlib.Ulog("%s: Business with designation %s does net exist\n", funcname, des)
+		biz, err = rlib.GetBusinessByDesignation(des)
+
+		if err != nil {
+			if rlib.IsSQLNoResultsError(err) {
+				rlib.Ulog("%s: Business with designation %s does net exist\n", funcname, des)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			rlib.Ulog("%s: DB error fetching business = %s\n", err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
 
+	fmt.Printf("found biz: BID = %d\n", biz.BID)
 	BuildXLedgerList(&ui, biz.BID, ui.DtStart, ui.DtStop)
 
 	w.Header().Set("Content-Type", "text/html")
@@ -47,7 +56,7 @@ func hndTrialBalance(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("%s: error loading template: %v\n", funcname, err)
 	}
 	fmt.Printf("%s - calling t.execute\n", funcname)
-	fmt.Printf("ui = %#v\n", ui)
+	// fmt.Printf("ui = %#v\n", ui)
 	err = t.Execute(w, &ui)
 	fmt.Printf("returned from t.Execute.\n")
 	if nil != err {
