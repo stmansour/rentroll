1639271e87f0967a57fd94fb65d3d2ae9152e637
Steve Mansour <sman@stevemansour.com>
2016-08-24 15:16:09 -0700

    fix call to mysql

--- test/setup/dosetup.sh
+++ test/setup/dosetup.sh
@@ -2,4 +2,4 @@
 
 #  Make sure that the test machine has the same dependent
 #  databases as the one(s) we're using to test with...
-/usr/local/bin/mysql --no-defaults accord < accord.sql 
+mysql --no-defaults accord < accord.sql 
