63eba5c1c9dddbbf9b3a614e8067bf76b43b2ccc
Steve Mansour <sman@stevemansour.com>
2016-10-03 09:52:55 -0700

    fixes for db init

--- activate.sh
+++ activate.sh
@@ -73,6 +73,8 @@ makeProdNode() {
 #--------------------------------------------------------------
 setupAppNode() {
 	# nothing to do at this time.  Phonebook (Directory) installs everything we need.
+	${GETFILE} accord/db/${DATABASENAME}db.sql.gz
+	gunzip ${DATABASENAME}db.sql
 	echo "DROP DATABASE IF EXISTS ${DATABASENAME}; CREATE DATABASE ${DATABASENAME}; USE ${DATABASENAME};" > restore.sql
 	echo "source ${DATABASENAME}db.sql" >> restore.sql
 	echo "GRANT ALL PRIVILEGES ON ${DATABASENAME} TO 'ec2-user'@'localhost' WITH GRANT OPTION;" >> restore.sql
