fe6fca480c1ebe25e074502c94af358eed13d962
Sudip Raval <sudip@auberginesolutions.com>
2016-12-14 18:27:30 +0530

    IMPROVE: code structure to write rentabletype data - separate file

--- importers/onesite/loader.go
+++ importers/onesite/loader.go
@@ -1,19 +1,16 @@
 package onesite
 
 import (
-	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	// "log"
+	"errors"
 	"os"
 	"path"
-	"reflect"
-	"rentroll/importers/core"
 	"rentroll/rcsv"
 	"rentroll/rlib"
 	"runtime"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -69,13 +66,21 @@ func GetOneSiteMapping(OneSiteFieldMap *CSVFieldMap) error {
 // as needed.
 func LoadOneSiteCSV(userSuppliedValues map[string]string) ([]error, string) {
 
-	// var errors
-	var errors []error
-	// msg to return
+	// var errors and msg to return
+	var errorList []error
 	var msg string
 
-	// this count used to skip number of rows from the very top of csv
-	var skipRowsCount int
+	// funcname
+	funcname := "LoadOneSiteCSV"
+
+	// get current timestamp used for creating csv files unique way
+	currentTime := time.Now()
+
+	// RFC3339Nano is const format defined in time package
+	// <FORMAT> = <SAMPLE>
+	// RFC3339Nano = "2006-01-02T15:04:05.999999999Z07:00"
+	// it is helpful while creating unique files
+	currentTimeFormat := currentTime.Format(time.RFC3339Nano)
 
 	// define column fields with order
 	const (
@@ -166,13 +171,44 @@ func LoadOneSiteCSV(userSuppliedValues map[string]string) ([]error, string) {
 	}
 	OneSiteColumnLength := len(OneSiteCSVHeaders)
 
-	// load csv file and get data from csv
-	t := rlib.LoadCSV(userSuppliedValues["OneSiteCSV"])
+	// load onesite mapping
+	var OneSiteFieldMap CSVFieldMap
+	err := GetOneSiteMapping(&OneSiteFieldMap)
+	if err != nil {
+		errorList = append(errorList, err)
+		msg = "Error while getting onesite field mapping"
+		return errorList, msg
+	}
+
+	// TODO: create all csv file here(rentable, custom attribute, people, rentable aggrement)
+
+	rentableTypeCSVFile, rentableTypeCSVWriter, ok :=
+		CreateRentableTypeCSV(
+			SplittedCSVStore, currentTimeFormat,
+			&OneSiteFieldMap.RentableTypeCSV,
+		)
+	if !ok {
+		// TODO: create errorlist in errors.go file to get error from that
+		errorList = append(errorList, errors.New("Unable To create file"))
+		return errorList, "Unable to create file"
+	}
+	defer rentableTypeCSVFile.Close()
+
+	// TODO: make all structures for duplicate data to avoid while writing data
+
+	// avoidDuplicateRentableTypeData used to keep track of rentableTypeData with Style field
+	// so that duplicate entries can be avoided while creating rentableType csv file
+	avoidDuplicateRentableTypeData := []string{}
 
 	// ================================
 	// First loop for validation on csv
 	// ================================
-	funcname := "LoadOneSiteCSV"
+
+	// load csv file and get data from csv
+	t := rlib.LoadCSV(userSuppliedValues["OneSiteCSV"])
+
+	// this count used to skip number of rows from the very top of csv
+	var skipRowsCount int
 
 	for i := 0; i < len(t); i++ {
 
@@ -195,8 +231,8 @@ func LoadOneSiteCSV(userSuppliedValues map[string]string) ([]error, string) {
 		if skipRowsCount > 0 {
 			x, vrs := rcsv.ValidateCSVColumns(csvCols, t[i][:OneSiteColumnLength], funcname, i)
 			if x > 0 {
-				errors = append(errors, vrs)
-				return errors, msg
+				errorList = append(errorList, vrs)
+				return errorList, msg
 			}
 		}
 	}
@@ -206,54 +242,6 @@ func LoadOneSiteCSV(userSuppliedValues map[string]string) ([]error, string) {
 	// Create csv files required for rentroll
 	// ================================
 	// in second round do split
-
-	// TODO: create all csv file here(rentable, custom attribute, people, rentable aggrement)
-
-	// get current timestamp used for creating csv files unique way
-	currentTime := time.Now()
-
-	// RFC3339Nano is const format defined in time package
-	// <FORMAT> = <SAMPLE>
-	// RFC3339Nano = "2006-01-02T15:04:05.999999999Z07:00"
-	// it is helpful while creating unique files
-	currentTimeFormat := currentTime.Format(time.RFC3339Nano)
-
-	// get onesite mapping
-	var OneSiteFieldMap CSVFieldMap
-	err := GetOneSiteMapping(&OneSiteFieldMap)
-	if err != nil {
-		errors = append(errors, err)
-		msg = "Error while getting onesite field mapping"
-		return errors, msg
-	}
-
-	// ======================================
-	// create rentabletypes file with current time
-	// ======================================
-	rentableTypeCSVFilePath := SplittedCSVStore + "/rentabletypes_" + currentTimeFormat + ".csv"
-	rentableTypeCSVFile, err := os.Create(rentableTypeCSVFilePath)
-	if err != nil {
-		errors = append(errors, err)
-		msg = fmt.Sprintf("Error while creating file %s with error (%s)", rentableTypeCSVFilePath, err)
-		return errors, msg
-	}
-	defer rentableTypeCSVFile.Close()
-
-	// create csv writer
-	rentableTypeCSVWriter := csv.NewWriter(rentableTypeCSVFile)
-
-	// parse headers of rentableTypeCSV using reflect
-	rentableTypeCSVHeaders := []string{}
-	rentableTypeCSVHeaders, ok := core.GetStructFields(&OneSiteFieldMap.RentableTypeCSV)
-	if !ok {
-		msg = "Unable to get struct fields for rentableTypeCSV"
-		return errors, msg
-	}
-	rentableTypeCSVWriter.Write(rentableTypeCSVHeaders)
-
-	// avoidDuplicateRentableTypeData used to keep track of rentableTypeData with Style field
-	// so that duplicate entries can be avoided while creating rentableType csv file
-	avoidDuplicateRentableTypeData := []string{}
 
 	for i := skipRowsCount + 1; i < len(t); i++ {
 		rowLoaded, csvRow := LoadOneSiteCSVRow(csvCols, t[i][:OneSiteColumnLength])
@@ -265,42 +253,19 @@ func LoadOneSiteCSV(userSuppliedValues map[string]string) ([]error, string) {
 			break
 		}
 
-		checkRentableTypeStyle := csvRow.FloorPlan
-		Stylefound := core.StringInSlice(checkRentableTypeStyle, avoidDuplicateRentableTypeData)
-		if Stylefound {
-			// jump to next row
-			// TODO: remove this jump as in future we need to include
-			// other type of parsing to create csv files
-			continue
-		} else {
-			avoidDuplicateRentableTypeData = append(avoidDuplicateRentableTypeData, checkRentableTypeStyle)
-		}
-
-		currentYear, _, _ := currentTime.Date()
-		DtStart := "1/1/" + strconv.Itoa(currentYear)
-		DtStop := "1/1/" + strconv.Itoa(currentYear+1)
-		// DtStart := time.Date(currentYear, 1, 1, 0, 0, 0, 0, currentTime.Location())
-		// DtStop := time.Date(currentYear+1, 1, 1, 0, 0, 0, 0, currentTime.Location())
-
-		// make rentableType data from userSuppliedValues and defaultValues
-		rentableTypeDefaultData := map[string]string{}
-		for k, v := range userSuppliedValues {
-			rentableTypeDefaultData[k] = v
-		}
-		rentableTypeDefaultData["DtStart"] = DtStart
-		rentableTypeDefaultData["DtStop"] = DtStop
-
-		// get csv row data
-		ok, rentableTypeCSVRow := GetRentableTypeCSVRow(
-			&csvRow, &OneSiteFieldMap.RentableTypeCSV,
-			currentTimeFormat, rentableTypeDefaultData,
+		// Write data to file of rentabletype
+		WriteRentableTypeCSVData(
+			rentableTypeCSVWriter,
+			&csvRow,
+			&avoidDuplicateRentableTypeData,
+			currentTime,
+			currentTimeFormat,
+			userSuppliedValues,
+			&OneSiteFieldMap.RentableTypeCSV,
 		)
-		fmt.Println(ok)
-		fmt.Println(rentableTypeCSVRow)
-		rentableTypeCSVWriter.Write(rentableTypeCSVRow)
+
 	}
-	rentableTypeCSVWriter.Flush()
-	return errors, msg
+	return errorList, msg
 }
 
 // RollBackSplitOperation func used to clear out the things
@@ -308,67 +273,6 @@ func LoadOneSiteCSV(userSuppliedValues map[string]string) ([]error, string) {
 //  and if any error occurs
 func RollBackSplitOperation(timestamp string) {
 	panic("RollBackSplitOperation")
-}
-
-// GetRentableTypeCSVRow used to create rentabletype
-// csv from onesite csv data to dump data via rcsv routine
-func GetRentableTypeCSVRow(
-	oneSiteRow *CSVRow,
-	fieldMap *core.RentableTypeCSV,
-	timestamp string,
-	DefaultValues map[string]string,
-) (bool, []string) {
-
-	// take initial variable
-	ok := false
-
-	// ======================================
-	// Load rentableType's data from onesiterow data
-	// ======================================
-	reflectedOneSiteRow := reflect.ValueOf(oneSiteRow).Elem()
-	reflectedRentableTypeFieldMap := reflect.ValueOf(fieldMap).Elem()
-
-	// length of RentableTypeCSV
-	rRTLength := reflectedRentableTypeFieldMap.NumField()
-
-	// return data array
-	dataMap := make(map[int]string)
-
-	for i := 0; i < rRTLength; i++ {
-		// get rentableType field
-		rentableTypeField := reflectedRentableTypeFieldMap.Type().Field(i)
-
-		// if rentableTypeField value exist in DefaultValues map
-		// then set it first
-		suppliedValue, found := DefaultValues[rentableTypeField.Name]
-		if found {
-			dataMap[i] = suppliedValue
-		}
-
-		// get mapping field if not found then panic error
-		MappedFieldName := reflectedRentableTypeFieldMap.FieldByName(rentableTypeField.Name).Interface().(string)
-		// MappedFieldName, ok := reflectedRentableTypeFieldMap.FieldByName(rentableTypeField.Name).Interface().(string)
-		// if !ok {
-		// 	panic("coudln't get mapping field")
-		// }
-
-		// if has not value then continue
-		if !reflectedOneSiteRow.FieldByName(MappedFieldName).IsValid() {
-			continue
-		}
-
-		// get field by mapping field name and then value
-		OneSiteFieldValue := reflectedOneSiteRow.FieldByName(MappedFieldName).Interface()
-		dataMap[i] = OneSiteFieldValue.(string)
-	}
-
-	dataArray := []string{}
-
-	for i := 0; i < rRTLength; i++ {
-		dataArray = append(dataArray, dataMap[i])
-	}
-
-	return ok, dataArray
 }
 
 // CSVHandler is main function to handle user uploaded
