aa4dcd0ee3e274978b3ea3a319663bfe0dd328ed
Sudip Raval <sudip@auberginesolutions.com>
2016-12-14 12:43:16 +0530

    MODIFIED: importers functional test dir structure

--- test/importers/onesite/functest.sh
+++ test/importers/onesite/functest.sh
@@ -3,22 +3,20 @@
 TESTNAME="Onesite Import"
 TESTSUMMARY="Tests initizing RentRoll DB from importing OneSite rentroll report."
 
-source ../share/base.sh
+RRBIN="../../../tmp/rentroll"
+source ../../share/base.sh
 
 docsvtest "a" "-b business.csv -L 3" "Business"
 docsvtest "b" "-T ratemplates.csv  -L 8,${BUD}" "RentalAgreementTemplates"
 
 # Sudip this is a quick hack to geth things into a known state
 # rework this code as needed so it is more production-ready
-rm -f /tmp/onesite/rentabletypes*.csv ./rentabletypes*.csv
+rm -f /tmp/onesite/rentabletypes_*.csv ./rentabletypes_*.csv
 
 ${RRBIN}/onesiteLoad -csv ./onesite.csv -bud ${BUD} >c 2>&1
 
 # Sudip  -- please update this code as needed.
 cp /tmp/onesite/*.csv .
-
-# Sudip -- i'm using rt2 as a manually fixed version of what your code produces
-#          after you fix things up, replace rt2.csv with the file your code generates
 
 # Sudip this is a quick hack to validate the file you're producing. Please
 # rework this code as needed so it is more production-ready
