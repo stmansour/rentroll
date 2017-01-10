1d606f2cea31894ace3dc221bfd0cc8fbeb10382
Sudip Raval <sudip@auberginesolutions.com>
2017-01-10 16:12:24 +0530

    FIX: add error with reference of unit, special validation for not occupied rentable

--- importers/onesite/csv_types.go
+++ importers/onesite/csv_types.go
@@ -67,46 +67,47 @@ var csvRowFieldRules = map[string]map[string]string{
 	"FloorPlan":       {"type": "string", "blank": "false"},
 	"UnitDesignation": {"type": "string", "blank": "true"},
 	"SQFT":            {"type": "uint", "blank": "false"},
-	"UnitLeaseStatus": {"type": "rentable_status", "blank": "true"},
-	"Name":            {"type": "string", "blank": "false"},
-	"PhoneNumber":     {"type": "phone", "blank": "true"},
-	"Email":           {"type": "email", "blank": "true"},
-	"MoveIn":          {"type": "date", "blank": "true"},
-	"NoticeForDate":   {"type": "string", "blank": "true"},
-	"MoveOut":         {"type": "date", "blank": "true"},
-	"LeaseStart":      {"type": "date", "blank": "false"},
-	"LeaseEnd":        {"type": "date", "blank": "false"},
-	"MarketAddl":      {"type": "float", "blank": "false"},
-	"DepOnHand":       {"type": "float", "blank": "true"},
-	"Balance":         {"type": "float", "blank": "true"},
-	"TotalCharges":    {"type": "float", "blank": "true"},
-	"Rent":            {"type": "float", "blank": "false"},
-	"WaterReImb":      {"type": "float", "blank": "true"},
-	"Corp":            {"type": "float", "blank": "true"},
-	"Discount":        {"type": "float", "blank": "true"},
-	"Platinum":        {"type": "float", "blank": "true"},
-	"Tax":             {"type": "float", "blank": "true"},
-	"ElectricReImb":   {"type": "float", "blank": "true"},
-	"Fire":            {"type": "float", "blank": "true"},
-	"ConcSpecl":       {"type": "float", "blank": "true"},
-	"WashDry":         {"type": "float", "blank": "true"},
-	"EmplCred":        {"type": "float", "blank": "true"},
-	"Short":           {"type": "float", "blank": "true"},
-	"PetFee":          {"type": "float", "blank": "true"},
-	"TrashReImb":      {"type": "float", "blank": "true"},
-	"TermFee":         {"type": "float", "blank": "true"},
-	"LakeView":        {"type": "float", "blank": "true"},
-	"Utility":         {"type": "float", "blank": "true"},
-	"Furn":            {"type": "float", "blank": "true"},
-	"Mtom":            {"type": "float", "blank": "true"},
-	"Referral":        {"type": "float", "blank": "true"},
+	// based on status value all of except this, will be validated
+	// so don't defined rule for status here
+	// "UnitLeaseStatus": {"type": "rentable_status", "blank": "true"},
+	"Name":          {"type": "string", "blank": "false"},
+	"PhoneNumber":   {"type": "phone", "blank": "true"},
+	"Email":         {"type": "email", "blank": "true"},
+	"MoveIn":        {"type": "date", "blank": "true"},
+	"NoticeForDate": {"type": "string", "blank": "true"},
+	"MoveOut":       {"type": "date", "blank": "true"},
+	"LeaseStart":    {"type": "date", "blank": "false"},
+	"LeaseEnd":      {"type": "date", "blank": "false"},
+	"MarketAddl":    {"type": "float", "blank": "false"},
+	"DepOnHand":     {"type": "float", "blank": "true"},
+	"Balance":       {"type": "float", "blank": "true"},
+	"TotalCharges":  {"type": "float", "blank": "true"},
+	"Rent":          {"type": "float", "blank": "false"},
+	"WaterReImb":    {"type": "float", "blank": "true"},
+	"Corp":          {"type": "float", "blank": "true"},
+	"Discount":      {"type": "float", "blank": "true"},
+	"Platinum":      {"type": "float", "blank": "true"},
+	"Tax":           {"type": "float", "blank": "true"},
+	"ElectricReImb": {"type": "float", "blank": "true"},
+	"Fire":          {"type": "float", "blank": "true"},
+	"ConcSpecl":     {"type": "float", "blank": "true"},
+	"WashDry":       {"type": "float", "blank": "true"},
+	"EmplCred":      {"type": "float", "blank": "true"},
+	"Short":         {"type": "float", "blank": "true"},
+	"PetFee":        {"type": "float", "blank": "true"},
+	"TrashReImb":    {"type": "float", "blank": "true"},
+	"TermFee":       {"type": "float", "blank": "true"},
+	"LakeView":      {"type": "float", "blank": "true"},
+	"Utility":       {"type": "float", "blank": "true"},
+	"Furn":          {"type": "float", "blank": "true"},
+	"Mtom":          {"type": "float", "blank": "true"},
+	"Referral":      {"type": "float", "blank": "true"},
 }
 
-// unOccupiedRentableBlankField holds the list of fields which maybe
-// allowed to be blank if rentable is unoccupied
-var unOccupiedRentableBlankField = []string{
-	"Name", "MoveIn", "MoveOut",
-	"LeaseStart", "LeaseEnd", "Rent",
+// notOccupiedRentableValidateFields holds the list of fields which must be
+// validated if rentable status is other than occupied
+var notOccupiedRentableValidateFields = []string{
+	"Unit", "FloorPlan", "SQFT", "MarketAddl",
 }
 
 // loadOneSiteCSVRow used to load data from slice
@@ -138,12 +139,37 @@ func validateOneSiteCSVRow(oneSiteCSVRow *CSVRow, rowIndex int) []error {
 	// fill data according to headers length
 	reflectedOneSiteCSVRow := reflect.ValueOf(oneSiteCSVRow).Elem()
 
-	for i := 0; i < len(csvCols); i++ {
-		fieldName := reflect.TypeOf(*oneSiteCSVRow).Field(i).Name
-		fieldValue := reflectedOneSiteCSVRow.Field(i).Interface().(string)
-		err := validateCSVField(oneSiteCSVRow, fieldName, fieldValue, rowIndex+1)
-		if err != nil {
-			rowErrs = append(rowErrs, err)
+	// perform validation based on rentable status
+	ok, _ := IsValidRentableStatus(oneSiteCSVRow.UnitLeaseStatus)
+
+	// TODO: need to clear on this what should importers do
+	// when it encounters other type of rentablestatus
+	// right now we are just throwing an error of bad value
+	if !ok {
+		statusErr := fmt.Errorf("\"%s\" has no valid rentable status value at row \"%d\" with unit \"%s\"",
+			"UnitDesignation", rowIndex, oneSiteCSVRow.Unit)
+		rowErrs = append(rowErrs, statusErr)
+		return rowErrs
+	}
+
+	if strings.Contains(oneSiteCSVRow.UnitLeaseStatus, "occupied") {
+		// if status is occupied then only perform validation over all fields
+		for i := 0; i < len(csvCols); i++ {
+			fieldName := reflect.TypeOf(*oneSiteCSVRow).Field(i).Name
+			fieldValue := reflectedOneSiteCSVRow.Field(i).Interface().(string)
+			err := validateCSVField(oneSiteCSVRow, fieldName, fieldValue, rowIndex+1)
+			if err != nil {
+				rowErrs = append(rowErrs, err)
+			}
+		}
+	} else {
+		// perform validation on fields defined in notOccupiedRentableValidateFields
+		for _, fieldName := range notOccupiedRentableValidateFields {
+			fieldValue := reflectedOneSiteCSVRow.FieldByName(fieldName).Interface().(string)
+			err := validateCSVField(oneSiteCSVRow, fieldName, fieldValue, rowIndex+1)
+			if err != nil {
+				rowErrs = append(rowErrs, err)
+			}
 		}
 	}
 
@@ -156,38 +182,20 @@ func validateCSVField(oneSiteCSVRow *CSVRow, field string, value string, rowInde
 
 	// if not found then simple return
 	if !ok {
+		// TODO: verify this also
 		return nil
 	}
 
 	fieldType, fieldBlankAllow := rule["type"], rule["blank"]
 
-	// ----------------- special rules comes first -----------------
-	//
-	// if status is not occupied then entry for name, phone, email, movein,
-	// lease start, lease end, rent would be null otherwise throw an error
-	if core.StringInSlice(field, unOccupiedRentableBlankField) {
-		if value == "" {
-			// check first rentable status is valid or not
-			// if not valid then just skip it
-			// for rentable status field, error will be count
-			ok, _ := IsValidRentableStatus(oneSiteCSVRow.UnitLeaseStatus)
-			if !ok {
-				return nil
-			}
-			// if status occupied and value is blank then throw an error
-			if strings.Contains(oneSiteCSVRow.UnitLeaseStatus, "occupied") {
-				return fmt.Errorf("\"%s\" has blank value at row \"%d\"", field, rowIndex)
-			}
-			return nil
-		}
-	} else if fieldBlankAllow == "true" && value == "" {
-		// check with blank rule
+	// check with blank rule
+	if fieldBlankAllow == "true" && value == "" {
 		return nil
 	}
 
 	// if blank is not allowed and value is blank then return with error
 	if fieldBlankAllow == "false" && value == "" {
-		return fmt.Errorf("\"%s\" has blank value at row \"%d\"", field, rowIndex)
+		return fmt.Errorf("\"%s\" has blank value at row \"%d\" with unit \"%s\"", field, rowIndex, oneSiteCSVRow.Unit)
 	}
 
 	// check with field type
@@ -195,47 +203,46 @@ func validateCSVField(oneSiteCSVRow *CSVRow, field string, value string, rowInde
 	case "int":
 		ok := core.IsIntString(value)
 		if !ok {
-			return fmt.Errorf("\"%s\" has no valid integer number value at row \"%d\"", field, rowIndex)
+			return fmt.Errorf("\"%s\" has no valid integer number value at row \"%d\" with unit \"%s\"", field, rowIndex, oneSiteCSVRow.Unit)
 		}
 		return nil
 	case "uint":
 		ok := core.IsUIntString(value)
 		if !ok {
-			return fmt.Errorf("\"%s\" has no valid positive integer number value at row \"%d\"", field, rowIndex)
+			return fmt.Errorf("\"%s\" has no valid positive integer number value at row \"%d\" with unit \"%s\"", field, rowIndex, oneSiteCSVRow.Unit)
 		}
 		return nil
 	case "float":
 		ok := core.IsFloatString(value)
 		if !ok {
-			return fmt.Errorf("\"%s\" has no valid integer number value at row \"%d\"", field, rowIndex)
+			return fmt.Errorf("\"%s\" has no valid integer number value at row \"%d\" with unit \"%s\"", field, rowIndex, oneSiteCSVRow.Unit)
 		}
 		return nil
 	case "email":
 		ok := core.IsValidEmail(value)
 		if !ok {
-			return fmt.Errorf("\"%s\" has no valid email value at row \"%d\"", field, rowIndex)
+			return fmt.Errorf("\"%s\" has no valid email value at row \"%d\" with unit \"%s\"", field, rowIndex, oneSiteCSVRow.Unit)
 		}
 		return nil
 	case "phone":
 		ok := core.IsValidPhone(value)
 		if !ok {
-			return fmt.Errorf("\"%s\" has no valid phone number value at row \"%d\"", field, rowIndex)
+			return fmt.Errorf("\"%s\" has no valid phone number value at row \"%d\" with unit \"%s\"", field, rowIndex, oneSiteCSVRow.Unit)
 		}
 		return nil
 	case "date":
 		_, err := rlib.StringToDate(value)
 		if err != nil {
-			return fmt.Errorf("\"%s\" has no valid date value at row \"%d\"", field, rowIndex)
+			return fmt.Errorf("\"%s\" has no valid date value at row \"%d\" with unit \"%s\"", field, rowIndex, oneSiteCSVRow.Unit)
 		}
 		return nil
 	case "rentable_status":
 		ok, _ := IsValidRentableStatus(value)
 		if !ok {
-			return fmt.Errorf("\"%s\" has no valid rentable status value at row \"%d\"", field, rowIndex)
+			return fmt.Errorf("\"%s\" has no valid rentable status value at row \"%d\" with unit \"%s\"", field, rowIndex, oneSiteCSVRow.Unit)
 		}
 		return nil
 	default:
 		return nil
 	}
-
 }
