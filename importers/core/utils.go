213316c293885b4450c8a6b8dca280240219142d
Sudip Raval <sudip@auberginesolutions.com>
2017-02-01 12:03:25 +0530

    REMOVE: unused code[validation, utils, struct_utils code] detected by codecoroner

--- importers/core/utils.go
+++ importers/core/utils.go
@@ -2,7 +2,6 @@ package core
 
 import (
 	"regexp"
-	"strconv"
 )
 
 // StringInSlice used to check whether string a
@@ -27,47 +26,10 @@ func IntegerInSlice(a int, list []int) bool {
 	return false
 }
 
-// IsIntString used to check whether string a
-// contains integer type value
-func IsIntString(a string) bool {
-	_, err := strconv.ParseInt(a, 10, 64)
-	if err != nil {
-		return false
-	}
-	return true
-}
-
-// IsUIntString used to check whether string a
-// contains integer type value
-func IsUIntString(a string) bool {
-	_, err := strconv.ParseUint(a, 10, 64)
-	if err != nil {
-		return false
-	}
-	return true
-}
-
-// IsFloatString used to check whether string a
-// contains float type value
-func IsFloatString(a string) bool {
-	_, err := strconv.ParseFloat(a, 64)
-	if err != nil {
-		return false
-	}
-	return true
-}
-
 // IsValidEmail used to check valid email or not
 func IsValidEmail(email string) bool {
 	// TODO: confirm which regex to use
 	// Re := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
 	Re := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+$`)
 	return Re.MatchString(email)
-}
-
-// IsValidPhone used to check valid phone or not
-func IsValidPhone(email string) bool {
-	// Re := regexp.MustCompile(`^[0-9+\-\)\(]{1,14}$`)
-	// return Re.MatchString(email)
-	return true
 }
