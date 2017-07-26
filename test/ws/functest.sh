afbfa63167b4090c631ddfb4a9a3ec037198d19b
Sudip Raval <sudip@auberginesolutions.com>
2017-07-27 00:17:22 +0530

    CHANGE: request body for rar, rau, rap save action

--- test/ws/functest.sh
+++ test/ws/functest.sh
@@ -109,11 +109,11 @@ dojsonPOST "http://localhost:8270/v1/asm/1/0" "request" "k"  "WebService--Insert
 dojsonGET "http://localhost:8270/v1/transactantstd/ISO?request=%7B%22search%22%3A%22s%22%2C%22max%22%3A250%7D" "l" "WebService--GetTransactantTypeDown"
 
 # Create a NEW Rentable User
-echo "%7B%22cmd%22%3A%22save%22%2C%22formname%22%3A%22tcidPicker%22%2C%22record%22%3A%7B%22recid%22%3A1%2C%22BID%22%3A%7B%22id%22%3A%22ISO%22%2C%22text%22%3A%22ISO%22%7D%2C%22TCID%22%3A373%2C%22RID%22%3A16%2C%22FirstName%22%3A%22Jason%22%2C%22MiddleName%22%3A%22%22%2C%22LastName%22%3A%22Thomas%22%2C%22DtStart%22%3A%223%2F5%2F2017%22%2C%22DtStop%22%3A%223%2F5%2F2018%22%7D%7D" > request
+echo "%7B%22cmd%22%3A%22save%22%2C%22formname%22%3A%22tcidPicker%22%2C%22record%22%3A%7B%22recid%22%3A1%2C%22BID%22%3A5%2C%22TCID%22%3A373%2C%22RID%22%3A16%2C%22FirstName%22%3A%22Jason%22%2C%22MiddleName%22%3A%22%22%2C%22LastName%22%3A%22Thomas%22%2C%22DtStart%22%3A%223%2F5%2F2017%22%2C%22DtStop%22%3A%223%2F5%2F2018%22%7D%7D" > request
 dojsonPOST "http://localhost:8270/v1/ruser/ISO/16" "request" "m"  "WebService--InsertARentableUser"
 
 # Create another NEW Rentable User -- same TCID
-echo "%7B%22cmd%22%3A%22save%22%2C%22formname%22%3A%22tcidPicker%22%2C%22record%22%3A%7B%22recid%22%3A1%2C%22BID%22%3A%7B%22id%22%3A%22ISO%22%2C%22text%22%3A%22ISO%22%7D%2C%22TCID%22%3A373%2C%22RID%22%3A16%2C%22FirstName%22%3A%22Jason%22%2C%22MiddleName%22%3A%22%22%2C%22LastName%22%3A%22Thomas%22%2C%22DtStart%22%3A%223%2F5%2F2017%22%2C%22DtStop%22%3A%223%2F5%2F2018%22%7D%7D" > request
+echo "%7B%22cmd%22%3A%22save%22%2C%22formname%22%3A%22tcidPicker%22%2C%22record%22%3A%7B%22recid%22%3A1%2C%22BID%22%3A5%2C%22TCID%22%3A373%2C%22RID%22%3A16%2C%22FirstName%22%3A%22Jason%22%2C%22MiddleName%22%3A%22%22%2C%22LastName%22%3A%22Thomas%22%2C%22DtStart%22%3A%223%2F5%2F2017%22%2C%22DtStop%22%3A%223%2F5%2F2018%22%7D%7D" > request
 dojsonPOST "http://localhost:8270/v1/ruser/ISO/16" "request" "n"  "WebService--InsertARentableUser"
 
 # Delete a Rentable User
@@ -121,7 +121,7 @@ echo "request%3D%7B%22cmd%22%3A%22delete%22%2C%22selected%22%3A%5B1%5D%2C%22limi
 dojsonPOST "http://localhost:8270/v1/ruser/ISO/16" "request" "o"  "WebService--DeleteARentableUser"
 
 # Create another NEW RAID Payor -- same TCID
-echo "%7B%22cmd%22%3A%22save%22%2C%22formname%22%3A%22tcidPicker%22%2C%22record%22%3A%7B%22recid%22%3A1%2C%22BID%22%3A%7B%22id%22%3A%22ISO%22%2C%22text%22%3A%22ISO%22%7D%2C%22TCID%22%3A367%2C%22RAID%22%3A16%2C%22FirstName%22%3A%22Eric%22%2C%22MiddleName%22%3A%22%22%2C%22LastName%22%3A%22Wilson%22%2C%22DtStart%22%3A%223%2F6%2F2017%22%2C%22DtStop%22%3A%223%2F6%2F2018%22%7D%7D" > request
+echo "%7B%22cmd%22%3A%22save%22%2C%22formname%22%3A%22tcidPicker%22%2C%22record%22%3A%7B%22recid%22%3A1%2C%22BID%22%3A5%2C%22TCID%22%3A367%2C%22RAID%22%3A16%2C%22FirstName%22%3A%22Eric%22%2C%22MiddleName%22%3A%22%22%2C%22LastName%22%3A%22Wilson%22%2C%22DtStart%22%3A%223%2F6%2F2017%22%2C%22DtStop%22%3A%223%2F6%2F2018%22%7D%7D" > request
 dojsonPOST "http://localhost:8270/v1/rapayor/ISO/16" "request" "p"  "WebService--InsertARAIDPayor"
 
 # Read RAID Payors
