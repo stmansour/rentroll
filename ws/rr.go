59a914ed60fc4166a5fb39ad18e648cf701c6aad
Sudip Raval <sudip@auberginesolutions.com>
2017-09-14 21:58:17 +0530

    DONE: Rentable - Assessment, Receipt Amount in grid records - rentroll report

--- ws/rr.go
+++ ws/rr.go
@@ -41,7 +41,7 @@ type RRGrid struct {
 	RentStop        rlib.NullDate   // stop date for Rent
 	Payors          rlib.NullString // payors list attached with this RA within same time
 	Users           rlib.NullString // users associated with the rentable
-	Sqft            int64           // rentable sq ft
+	Sqft            rlib.NullInt64  // rentable sq ft
 	Description     rlib.NullString
 	GSR             rlib.NullFloat64
 	PeriodGSR       rlib.NullFloat64
@@ -191,7 +191,7 @@ func SvcRR(w http.ResponseWriter, r *http.Request, d *ServiceData) {
 		"DtStop":       d.wsSearchReq.SearchDtStop.Format(rlib.RRDATEFMTSQL),
 	}
 
-	// get TOTAL COUNT First
+	/*// get TOTAL COUNT First
 	countQuery := renderSQLQuery(rentalAgrQuery, qc)
 	g.Total, err = GetQueryCount(countQuery, qc)
 	if err != nil {
@@ -199,7 +199,7 @@ func SvcRR(w http.ResponseWriter, r *http.Request, d *ServiceData) {
 		SvcGridErrorReturn(w, err, funcname)
 		return
 	}
-	rlib.Console("g.Total = %d\n", g.Total)
+	rlib.Console("g.Total = %d\n", g.Total)*/
 
 	// FETCH the records WITH LIMIT AND OFFSET
 	limitAndOffsetClause := `
@@ -223,7 +223,6 @@ func SvcRR(w http.ResponseWriter, r *http.Request, d *ServiceData) {
 	count := 0
 	for rows.Next() {
 		var q RRGrid
-		q.Recid = i
 		q.BID = d.BID
 
 		// get records info in struct q
@@ -237,7 +236,8 @@ func SvcRR(w http.ResponseWriter, r *http.Request, d *ServiceData) {
 		if len(xbiz.RT[q.RTID].CA) > 0 { // if there are custom attributes
 			c, ok := xbiz.RT[q.RTID].CA[custom] // see if Square Feet is among them
 			if ok {                             // if it is...
-				q.Sqft, err = rlib.IntFromString(c.Value, "invalid sqft of custom attribute")
+				sqft, err := rlib.IntFromString(c.Value, "invalid sqft of custom attribute")
+				q.Sqft.Scan(sqft)
 				if err != nil {
 					SvcGridErrorReturn(w, err, funcname)
 					return
@@ -254,9 +254,31 @@ func SvcRR(w http.ResponseWriter, r *http.Request, d *ServiceData) {
 		// now get assessment, receipt related info
 		asmRcptQC["WhereClause"] = fmt.Sprintf("Rentable.BID=%d AND Rentable.RID=%d", q.BID, q.RID)
 		arQry := renderSQLQuery(asmRcptQuery, asmRcptQC) // get formatted query with substitution of select, where, order clause
-		rlib.Console("Assessment + Receipt AMOUNT db query = %s\n", arQry)
+		rlib.Console("Rentable : Assessment + Receipt AMOUNT db query = %s\n", arQry)
+		// hold RRGrid in slice
+		var rentableResult = []RRGrid{}
+		// execute the query
+		arRows, err := rlib.RRdb.Dbrr.Query(arQry)
+		arCount := 0
+		if err == nil {
+			for arRows.Next() {
+				if arCount > 0 { // if more than one rows per rentable then create new RRGrid struct
+					var nq = RRGrid{RID: q.RID}
+					_ = arRows.Scan(&nq.Description, &nq.AmountDue, &nq.PaymentsApplied) // ignore error
+					nq.Recid = g.Total
+					rentableResult = append(rentableResult, nq)
+				} else {
+					_ = arRows.Scan(&q.Description, &q.AmountDue, &q.PaymentsApplied) // ignore error
+					q.Recid = g.Total
+					rentableResult = append(rentableResult, q)
+				}
+				arCount++
+				g.Total++ // grid rows count
+			}
+		}
+		arRows.Close()
 
-		g.Records = append(g.Records, q)
+		g.Records = append(g.Records, rentableResult...)
 		count++ // update the count only after adding the record
 		if count >= d.wsSearchReq.Limit {
 			break // if we've added the max number requested, then exit
