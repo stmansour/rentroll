89305e9d1d7472c5d2988094f13648cb8e049035
Steve Mansour <sman@stevemansour.com>
2017-09-21 00:54:45 -0700

    ADD: new file

---
+++ rlib/rarbal.go
@@ -0,0 +1,124 @@
+package rlib
+
+import "time"
+
+// RARStmtEntry descriGetRARBalancebes an entry on a statement
+type RARStmtEntry struct {
+	T       int                // 1 = assessment, 2 = Receipt
+	A       *Assessment        // for type==1, the pointer to the assessment
+	R       *ReceiptAllocation // for type ==2, the pointer to the receipt
+	Amt     float64            // amount of the receipt or assessment
+	Reverse bool               // is this a reversal?
+	Dt      time.Time          // date/time of this assessment or receipt
+	TCID    int64              // IF THIS IS FOR A PAYOR STATEMENT, the TCID of the Payor, otherwise 0
+}
+
+// GetBeginEndRARBalance gets the balance associated with a Rentable and a
+// Rental Agreement at a particular point in time.
+//
+// INPUTS
+//   rid  - RID of Rentable
+//   raid - RAID of Rental Agreement
+//   d1   - time for which balance is requested
+//   d2   - time for which balance is requested
+//
+// RETURNS
+//   float64 - the balance for the Rentable rid in Rental Agreement raid at
+//             time dt
+//   error   - any error encountered
+//-----------------------------------------------------------------------------
+func GetBeginEndRARBalance(rid, raid int64, d1, d2 *time.Time) (float64, float64, error) {
+	var err error
+	begin := float64(0)
+	end := float64(0)
+	begin, err = GetRARBalance(rid, raid, d1)
+	end, err = GetRARBalance(rid, raid, d2)
+	return begin, end, err
+}
+
+// GetRARBalance gets the balance associated with a Rentable and a
+// Rental Agreement at a particular point in time.
+//
+// INPUTS
+//   bid     - biz id
+//   rid     - RID of Rentable
+//   raid    - RAID of Rental Agreement
+//   dt      - time for which balance is requested
+//
+// RETURNS
+//   float64 - the balance for the Rentable rid in Rental Agreement raid at
+//             time dt
+//   error   - any error encountered
+//-----------------------------------------------------------------------------
+func GetRARBalance(rid, raid int64, dt *time.Time) (float64, error) {
+	funcname := "GetRARBalance"
+	bal := float64(0)
+
+	lm := GetRARentableLedgerMarkerOnOrBefore(raid, rid, dt)
+	if lm.LMID == 0 {
+		LogAndPrint("%s: Could not find LedgerMarker for RAID=%d, RID=%d, on or before %s\n",
+			funcname, raid, rid, dt.Format(RRDATEFMT3))
+		return bal, nil
+	}
+
+	//------------------------------------------------------------------
+	// Get all the assessments and payments for this RAID, RID pair...
+	//------------------------------------------------------------------
+	bal = lm.Balance + GetRARAcctRange(raid, rid, &lm.Dt, dt)
+	return bal, nil
+}
+
+// GetRARAcctRange returns the change in balance for the supplie RAID,RID
+// combination over the supplied time range.
+//
+// INPUTS
+//   raid - RAID of Rental Agreement
+//   rid  - RID of Rentable
+//   d1   - time for which balance is requested
+//   d2   - time for which balance is requested
+//
+// RETURNS
+//   float64 - the balance for the Rentable rid in Rental Agreement raid at
+//             time dt
+//   error   - any error encountered
+//-----------------------------------------------------------------------------
+func GetRARAcctRange(raid, rid int64, d1, d2 *time.Time) float64 {
+	funcname := "GetRARAcctRange"
+	Console("Entered %s\n", funcname)
+	bal := float64(0)
+
+	//----------------------------------------------------------------
+	// Total all assessments in the supplied range that involve RAID.
+	//----------------------------------------------------------------
+	Console("%s: Will QUERY ASSESSMENTS FOR  raid=%d,rid=%d,%s - %s\n", funcname, raid, rid, d1.Format(RRDATEFMT3), d2.Format(RRDATEFMT3))
+	Console("q = SELECT * FROM Assessments WHERE (RentCycle=0  OR (RentCycle>0 AND PASMID>0)) AND RAID=%d AND RID=%d AND Stop>=%q AND Start<%q\n", raid, rid, d1.Format(RRDATEFMTSQL), d2.Format(RRDATEFMTSQL))
+	rows, err := RRdb.Prepstmt.GetAssessmentsByRARRange.Query(raid, rid, d1, d2)
+	Errcheck(err)
+	defer rows.Close()
+	for rows.Next() {
+		var a Assessment
+		ReadAssessments(rows, &a)
+		if 0 == a.FLAGS&0x4 { // if this is not a reversal...
+			bal += a.Amount // ... then add it to the balance
+		}
+		Console("\tASMID = %d\n", a.ASMID)
+
+		//----------------------------------------------------------------
+		// Total all receipts applied toward this ASMID
+		//----------------------------------------------------------------
+		innerRows, err := RRdb.Prepstmt.GetASMReceiptAllocationsInRARDateRange.Query(raid, a.ASMID, d1, d2)
+		Errcheck(err)
+		defer innerRows.Close()
+		for innerRows.Next() {
+			var ra ReceiptAllocation
+			ReadReceiptAllocations(innerRows, &ra)
+			a, err := GetAssessment(ra.ASMID)
+			Errcheck(err)
+			if 0 == a.FLAGS&0x4 {
+				bal -= ra.Amount
+			}
+		}
+	}
+
+	return bal
+}
