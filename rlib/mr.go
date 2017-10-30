ecea61d28014cb361faecdab8491299da14c193e
Sudip Raval <sudip@auberginesolutions.com>
2017-10-30 12:49:28 +0530

    WIP: hanlding multiple market rates

--- rlib/mr.go
+++ rlib/mr.go
@@ -21,9 +21,8 @@ func DaysInMonth(year int, m time.Month) int {
 func GetMRAmtInDateRange(rmridList []int64, startDt, stopDt time.Time) (amt float64, err error) {
 	const funcname = "GetMRAmtInDateRange"
 	var (
-		d         = startDt
 		breakLoop = false
-		nmd       time.Time // next month first date
+		ied       time.Time // interval end date
 		mr        RentableMarketRate
 	)
 	Console("Entered in %s\n", funcname)
@@ -38,35 +37,45 @@ func GetMRAmtInDateRange(rmridList []int64, startDt, stopDt time.Time) (amt floa
 		if err != nil {
 			return
 		}
-		Console("Amount: %f, RMRID: %d, RTID: %d, startDt: %q, stopDt: %q\n",
-			mr.MarketRate, mr.RMRID, mr.RTID, startDt, stopDt)
+		Console("Amount: %f, RMRID: %d, RTID: %d, startDt: %q, stopDt: %q, DtStart: %q, DtStop: %q\n",
+			mr.MarketRate, mr.RMRID, mr.RTID, startDt, stopDt, mr.DtStart, mr.DtStop)
+
+		// if market rate time range not overlaps then continue...
+		if !DateRangeOverlap(&mr.DtStart, &mr.DtStop, &startDt, &stopDt) {
+			continue
+		}
+
+		isd := mr.DtStart // interval start date
+		if mr.DtStart.Before(startDt) {
+			isd = startDt
+		}
 
 		for {
 			yDiff, mDiff := 0, 1 // year would be same, month would be next (MOSTLY)
-			if d.Month() == time.December {
+			if isd.Month() == time.December {
 				yDiff = 1 // d is being updated
 				mDiff = 0
 			}
 
 			// jump to next month Start
-			Console("\n\nCurrent Date: %q\n", d)
-			nmd = d.AddDate(yDiff, mDiff, 1)
-			Console("Next Date: %q\n\n\n", nmd)
+			Console("\n\nCurrent Date: %q\n", isd)
+			ied = isd.AddDate(yDiff, mDiff, 1)
+			Console("Next Date: %q\n\n\n", ied)
 
-			if nmd.After(stopDt) { // if nmd after stopDt then stop
-				nmd = stopDt
+			if ied.After(stopDt) { // if ied after stopDt then stop
+				ied = stopDt
 				breakLoop = true
-				Console("Yes, break the loop with nmd: %q\n", nmd)
+				Console("Yes, break the loop with ied: %q\n", ied)
 			}
 
-			diff := nmd.Sub(d)
+			diff := ied.Sub(isd)
 			daysDiff := int(diff.Hours() / 24)
-			perDayMR := mr.MarketRate / float64(DaysInMonth(d.Year(), d.Month()))
+			perDayMR := mr.MarketRate / float64(DaysInMonth(isd.Year(), isd.Month()))
 			amt += float64(daysDiff) * perDayMR // number of days * per day mr
-			Console("daysDiff: %d, d.Diff:%d\n", daysDiff, DaysInMonth(d.Year(), d.Month()))
+			Console("daysDiff: %d, d.Diff:%d\n", daysDiff, DaysInMonth(isd.Year(), isd.Month()))
 			Console("Per day marketRate: %f", perDayMR)
 
-			d = nmd // update d with nmd (next month day)
+			isd = ied // update d with ied (next month day)
 			if breakLoop {
 				break
 			}
