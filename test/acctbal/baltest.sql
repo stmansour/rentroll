67be27799ff2ab0b5e0372ef6df0a5bf59658f02
Sudip Raval <sudip@auberginesolutions.com>
2017-11-01 15:09:07 +0530

    FIX: ReceiptAllocation change reflect in all databases

--- test/acctbal/baltest.sql
+++ test/acctbal/baltest.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.16, for osx10.12 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
 --
 -- Host: localhost    Database: rentroll
 -- ------------------------------------------------------
--- Server version	5.7.16
+-- Server version	5.7.20-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -1413,7 +1413,7 @@ CREATE TABLE `ReceiptAllocation` (
   `Amount` decimal(19,4) NOT NULL DEFAULT '0.0000',
   `ASMID` bigint(20) NOT NULL DEFAULT '0',
   `FLAGS` bigint(20) NOT NULL DEFAULT '0',
-  `AcctRule` varchar(150) DEFAULT NULL,
+  `AcctRule` varchar(150) NOT NULL DEFAULT '',
   `LastModTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `LastModBy` bigint(20) NOT NULL DEFAULT '0',
   `CreateTS` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -2237,4 +2237,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-30  0:43:02
+-- Dump completed on 2017-11-01 15:04:15
