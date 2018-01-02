10fe30727533838bf15e73c6610c085731a69920
Sudip Raval <sudip@auberginesolutions.com>
2018-01-02 17:49:14 +0530

    FIX: use Rcsv.DtStart, Rcsv.DtStop instead of ri dates, need to mark 'NeedsDt' f in rrloadcsv main.go

--- rrpt/invoice.go
+++ rrpt/invoice.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"gotable"
+	"rentroll/rcsv"
 	"rentroll/rlib"
 	"strings"
 )
@@ -141,7 +142,9 @@ func RRreportInvoices(ctx context.Context, ri *ReporterInfo) string {
 	tbl.AddColumn("DeliveredBy", 10, gotable.CELLSTRING, gotable.COLJUSTIFYLEFT)
 
 	// TODO(Steve): should we replce Rcsv.DtStart, DtStop with "ri" dates?
-	m, err := rlib.GetAllInvoicesInRange(ctx, ri.Bid, &ri.D1, &ri.D2)
+	// However, ri dates need the flag on of NeedsDt in main.go
+	m, err := rlib.GetAllInvoicesInRange(ctx, ri.Bid, &rcsv.Rcsv.DtStart, &rcsv.Rcsv.DtStop)
+	// m, err := rlib.GetAllInvoicesInRange(ctx, ri.Bid, &ri.D1, &ri.D2)
 	if err != nil {
 		tbl.SetSection3(err.Error())
 		return tbl.String()
