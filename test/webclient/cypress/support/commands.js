9d597b13f97389678bcbdd26c8a7f083b216b292
akshaybosamiya <akshay@auberginesolutions.com>
2018-01-24 12:52:50 +0530

    UPDATE: commands,js

--- test/webclient/cypress/support/commands.js
+++ test/webclient/cypress/support/commands.js
@@ -1,39 +1,6 @@
 // ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
+// This file contains custom build command for cypress
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
-/*  cy
-    .contains("Sign in", {log: false})
-    .get("#user", {log: false}).type(email, {log: false})
-    .get("#password", {log: false}).type(password, {log: false})
-    .get("button", {log: false}).click({log: false}) //this should submit the form
-    .get("h1", {log: false}).contains("Dashboard", {log: false}) //we should be on the dashboard now
-    .url({log: false}).should("match", /dashboard/, {log: false})
-    .then(function(){
-      log.snapshot().end();
-    });*/
 
 // -- This is a custom command to login int AIR application --
 Cypress.Commands.add("login", function(){
