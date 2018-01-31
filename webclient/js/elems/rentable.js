db1ab6b48ee0d3c41c2dd6e2739d9f8633b0afd2
Sudip Raval <sudip@auberginesolutions.com>
2018-01-31 11:27:33 +0530

    rentableForm: remove entry if {UseStatus, LeaseStatus}=unknonwn in rentableStatusGrid onSave event

--- webclient/js/elems/rentable.js
+++ webclient/js/elems/rentable.js
@@ -460,11 +460,20 @@ function buildRentableElements() {
             g.add(newRec);
         },
         onSave: function(event) {
+            // TODO(Sudip): validation on values before sending these to server
+
             this.records.forEach(function(item, index, arr) {
                 arr[index].UseStatus = parseInt(arr[index].UseStatus);
                 arr[index].LeaseStatus = parseInt(arr[index].LeaseStatus);
+
+                // if UseStatus and LeaseStatus both kept as "unknown" then it doesn't
+                // make sense to send this entry to server, remove it
+                if (UseStatus === 0 && LeaseStatus === 0) {
+                    arr.splice(index, 1);
+                }
+
                 /*// if dateMode is set, then add one day to the stopDate
-                if (app.dateMode) {
+                if (app.dateMode === 1) {
                     var d = dateFromString(arr[index].DtStop);
                     d.setDate(d.getDate() + 1);
                     arr[index].DtStop = dateFmtStr(d);
@@ -672,12 +681,15 @@ function buildRentableElements() {
             g.add(newRec);
         },
         onSave: function(event) {
+            // TODO(Sudip): validation on values before sending these to server
+
             this.records.forEach(function(item, index, arr) {
                 arr[index].OverrideRentCycle = parseInt(arr[index].OverrideRentCycle);
                 arr[index].OverrideProrationCycle = parseInt(arr[index].OverrideProrationCycle);
                 arr[index].RTID = parseInt(arr[index].RTID);
+
                 /*// if dateMode is set, then add one day to the stopDate
-                if (app.dateMode) {
+                if (app.dateMode === 1) {
                     var d = dateFromString(arr[index].DtStop);
                     d.setDate(d.getDate() + 1);
                     arr[index].DtStop = dateFmtStr(d);
