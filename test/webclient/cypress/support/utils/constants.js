4f79b5248a73f32ac5f046349069cf0be436321c
akshaybosamiya <akshay@auberginesolutions.com>
2018-02-06 17:59:07 +0530

    UPDATE: Test for Payor, Resident Addres, Comment columns
    WIP: We're skipping ERentableName(Resident Address) field in form.
         Because it have complex rendering in DOM with multi item list.

--- test/webclient/cypress/support/utils/constants.js
+++ test/webclient/cypress/support/utils/constants.js
@@ -31,6 +31,11 @@ export const PAGE_LOAD_TIME = 2000;
 export const LOGIN_WAIT_TIME = 2000;
 
 // select date
-export let fromDate = "8/1/2017";
-export let month = "7"; // If Aug(8) than month is 7 (8-1). Index start from 0
-export let year = "2017";
+export let fromDate = "1/1/2018";
+export let fromMonth = "0"; // If Aug(8) than month is 7 (8-1). Index start from 0
+export let fromYear = "2018";
+
+// select to date
+export let toDate = "2/1/2018";
+export let toMonth = "1"; // Feb
+export let toYear = "2018";
