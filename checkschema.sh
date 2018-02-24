0a0cf578fc9c1d30e6adac31c0527c0f5c3d2b1a
Steve Mansour <sman@stevemansour.com>
2018-02-23 19:42:56 -0800

    UPDATE: formatting on schema notice

--- checkschema.sh
+++ checkschema.sh
@@ -3,7 +3,11 @@ pushd tools/schemacmp
 ./schcmp.sh >w 2>&1
 e=$(grep TABLE report.txt | wc -l)
 if [ ${e} -gt 0 ]; then
-    echo "*** NOTICE ***  Differences Count between local schema and production: ${e}"
+    echo "*************************************************************************************"
+    echo "****"
+    echo "****  NOTICE  ***  Differences Count between local schema and production: ${e}"
+    echo "****"
+    echo "*************************************************************************************"
     echo "See ./tools/schemacmp/report.txt for details"
 else
     echo "Schema on local machine and production are the same.  PASS"
