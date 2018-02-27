7d2a688b1d04b1899eb5b118cf01a8bfe6adbc98
Steve Mansour <sman@stevemansour.com>
2018-02-26 19:05:41 -0800

    UPDATE: minor formatting for build output

--- checkschema.sh
+++ checkschema.sh
@@ -2,15 +2,16 @@
 pushd tools/schemacmp
 ./schcmp.sh >w 2>&1
 e=$(grep TABLE report.txt | wc -l)
+echo "*************************************************************************************"
 if [ ${e} -gt 0 ]; then
-    echo "*************************************************************************************"
     echo "****"
     echo "****  NOTICE  ***  Differences Count between local schema and production: ${e}"
     echo "****"
-    echo "*************************************************************************************"
     echo "See ./tools/schemacmp/report.txt for details"
 else
-    echo "Schema on local machine and production are the same.  PASS"
+    echo "***   Schema on local machine and Roller production are the same."
+    echo "***   The build bundle will be publised to the repo."
 fi
+echo "*************************************************************************************"
 rm -f w
 popd
