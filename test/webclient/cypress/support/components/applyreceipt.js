4c38357d7e15909e5b195982e678404b7742766d
akshaybosamiya <akshay@auberginesolutions.com>
2018-03-08 15:46:56 +0530

    WIP: Cypress UI Tests for Apply Receipt section

---
+++ test/webclient/cypress/support/components/applyreceipt.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const GRID = "allocfundsGrid";
+const SIDEBAR_ID = "allocfunds";
+const FORM = "allocfundsForm";
+const MODULE = "allocfunds";
+
+// Below configurations are in use while performing tests via roller_spec.js for AIR Roller application
+// For Module: Deposit accounts
+export let conf = {
+    grid: GRID,
+    form: FORM,
+    sidebarID: SIDEBAR_ID,
+    module: MODULE,
+    endPoint: "/{0}/allocfunds/{1}",
+    methodType: 'POST',
+    excludeGridColumns: [],
+    buttonNamesInForm: [],
+    notVisibleButtonNamesInForm: ["close"],
+    buttonNamesInDetailForm: ["save"],
+    skipColumns: [],
+    skipFields: [],
+    primaryId: "TCID",
+    haveDateValue: false,
+};
+
+//TODO(Akshay): Find button in form
+
