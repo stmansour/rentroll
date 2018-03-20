b765ec0c677553d82641d0637c7560a53fa618f1
akshaybosamiya <akshay@auberginesolutions.com>
2018-03-20 17:15:07 +0530

    Update: todo list /test/webclient

--- test/webclient/cypress/support/components/rentroll.js
+++ test/webclient/cypress/support/components/rentroll.js
@@ -19,5 +19,3 @@ export let conf = {
     fromDate: new Date(2018, 2, 1), // year, month-1, day : 1st March 2018
     toDate: new Date(2018, 3, 1) // 1st April 2018
 };
-
-// TODO(Akshay): Tests for Use Period and Rent Period columns
