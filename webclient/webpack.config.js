6c2fd582110cc6c90edbb91dc29c5150efbedabb
akshaybosamiya <akshay@auberginesolutions.com>
2018-03-23 16:39:37 +0530

    UPDATE: Webpack configuration

--- webclient/webpack.config.js
+++ webclient/webpack.config.js
@@ -7,11 +7,17 @@ var glob = require('glob');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const sourcePath = path.join(__dirname, './js');
+const sourcePath = path.join(__dirname, './js/elems');
+const outPath = path.join(__dirname, './js');
 
 const config = {
     context: sourcePath,
-    entry: 'bundle.js',
+    entry: ['account.js', 'allocFunds.js', 'ar.js', 'asms.js', 'bpwrapper.js', 'datenav.js', 'datetimeutil.js', 'depmeth.js', 'deposit.js', 'depository.js', 'dirtyforms.js', 'expenses.js', 'init.js', 'layout.js', 'ledger.js', 'login.js', 'notes.js', 'pmt.js', 'ra.js', 'rapicker.js', 'receipt.js', 'rentable.js', 'report.js', 'rovreceipt.js', 'rr.js', 'rt.js', 'rutil.js', 'sidebar.js', 'statements.js', 'stmtpayor.js', 'transactant.js', 'tws.js'],
+    output: {
+        path: outPath,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
     target: 'web',
     resolve: {
         extensions: ['.js'],
@@ -21,7 +27,7 @@ const config = {
         rules: [
             {
                 // test: "/home/akshay/go/src/rentroll/webclient/js/bundle.js",
-                test: /rovreceipt.js$/,
+                test: /\.js$/,
                 loader: ['istanbul-instrumenter-loader']
             }
         ]
@@ -43,8 +49,7 @@ const config = {
         fs: 'empty',
         net: 'empty'
     },
-    watch: true,
-    mode: 'development'
+    mode: 'development',
 };
 
 module.exports = config;
