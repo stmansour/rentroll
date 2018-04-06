0eb20e2fb3f565fa9c1dc111aa72507701a2c6b3
Steve Mansour <sman@stevemansour.com>
2018-04-06 15:52:23 -0700

    UPDATE: task list definition updates. WIP

--- webclient/js/elems/tld.js
+++ webclient/js/elems/tld.js
@@ -3,7 +3,8 @@
     w2ui, $, app, console,setToTLDForm,
     form_dirty_alert, addDateNavToToolbar, w2utils, w2popup,
     popupTaskDescForm, ensureSession, dtFormatISOToW2ui,
-    dtFormatISOToW2ui, localtimeToUTC,
+    dtFormatISOToW2ui, localtimeToUTC, setDefaultFormFieldAsPreviousRecord,
+    getTLDInitRecord, getCurrentBID,
 */
 
 // Temporary storage for when a date is toggled off
@@ -16,6 +17,51 @@ var TLData = {
     sEpochDue: '',
     sEpochPreDue: '',
 };
+
+window.getTLDInitRecord = function (BID, previousFormRecord){
+    var y = new Date();
+    var y1 = new Date( y.getFullYear(), y.getMonth(), 1);
+    var month = (y.getMonth() + 1) % 12;
+    var epochPreDue = new Date(y.getFullYear(), y.getMonth(), 20);
+    var epochDue = new Date(y.getFullYear(), month, 0); 
+
+    // var Cycle;
+    // for (var i = 0; i < app.cycleFreq.length; i++) {
+    //     if (app.cycleFreq[i] === "Monthly") {
+    //         Cycle = i;
+    //     }
+    // }
+
+    var defaultFormData = {
+        TLDID: 0,
+        BID: BID,
+        Name: '',
+        Cycle: 6,
+        ChkEpochDue: true,
+        ChkEpochPreDue: true,
+        Epoch: dtFormatISOToW2ui(y1.toString()),
+        EpochDue: dtFormatISOToW2ui(epochDue.toString()),
+        EpochPreDue: dtFormatISOToW2ui(epochPreDue.toString()),
+        FLAGS: 0,
+        Comment: '',
+        CreateTS: y.toString(),
+        CreateBy: app.uid,
+        LastModTime: y.toString(),
+        LastModBy: app.uid,
+    };
+
+    // if called after 'save and add another' action there previous form record is passed as Object
+    // else it is null
+    if ( previousFormRecord ) {
+        defaultFormData = setDefaultFormFieldAsPreviousRecord(
+            [ 'ChkEpochDue', 'ChkEpochPreDue', 'EpochDue', 'EpochPreDue'], // Fields to Reset
+            defaultFormData,
+            previousFormRecord
+        );
+    }
+    return defaultFormData;
+};
+
 
 window.buildTaskListDefElements = function () {
     //------------------------------------------------------------------------
@@ -68,6 +114,20 @@ window.buildTaskListDefElements = function () {
                 // warn user if form content has been changed
                 form_dirty_alert(yes_callBack, no_callBack, yes_args, no_args);
             };
+        },
+        onAdd: function (/*event*/) {
+            var yes_args = [this],
+                no_callBack = function() { return false; },
+                yes_callBack = function(grid) {
+                    var BID = getCurrentBID();
+                    // reset it
+                    app.last.grid_sel_recid = -1;
+                    grid.selectNone();
+                    setToTLDForm(BID, 0, app.D1, app.D2);
+                };
+
+                // warn user if form content has been changed
+                form_dirty_alert(yes_callBack, no_callBack, yes_args);
         },
     });
 
@@ -183,7 +243,6 @@ window.buildTaskListDefElements = function () {
                     }
                     f.refresh();
                     break;
-
                 }
             };
         },
@@ -199,6 +258,21 @@ window.buildTaskListDefElements = function () {
         url: '/v1/tds/',
         multiSelect: false,
         postData: {searchDtStart: app.D1, searchDtStop: app.D2, Bool1: app.PayorStmtExt},
+        show: {
+            toolbar         : true,
+            footer          : false,
+            toolbarAdd      : true,   // indicates if toolbar add new button is visible
+            toolbarDelete   : false,   // indicates if toolbar delete button is visible
+            toolbarSave     : false,   // indicates if toolbar save button is visible
+            selectColumn    : false,
+            expandColumn    : false,
+            toolbarEdit     : false,
+            toolbarSearch   : false,
+            toolbarInput    : false,
+            searchAll       : false,
+            toolbarReload   : false,
+            toolbarColumns  : true,
+        },
         columns: [
             { field: 'recid',       caption: 'recid',       size: '35px',  sortable: true, hidden: true},
             { field: 'TDID',        caption: 'TDID',        size: '35px',  sotrable: true, hidden: true},
@@ -399,8 +473,8 @@ window.buildTaskListDefElements = function () {
         padding: 0,
         panels: [
             { type: 'left',    size: 0,     hidden: true },
-            { type: 'top',     size: '35%', hidden: false, content: 'top',  resizable: true, style: app.pstyle },
-            { type: 'main',    size: '65%', hidden: false, content: 'main', resizable: true, style: app.pstyle },
+            { type: 'top',     size: '30%', hidden: false, content: 'top',  resizable: true, style: app.pstyle },
+            { type: 'main',    size: '70%', hidden: false, content: 'main', resizable: true, style: app.pstyle },
             { type: 'preview', size: 0,     hidden: true,  content: 'PREVIEW'  },
             { type: 'bottom',  size: 50,    hidden: false, content: 'bottom', resizable: false, style: app.pstyle },
             { type: 'right',   size: 0,     hidden: true }
@@ -472,12 +546,17 @@ window.setToTLDForm = function (bid, id, d1,d2) {
         w2ui.tldsInfoForm.header = 'Task List Definition ' + id;
         w2ui.tldsInfoForm.request();
 
-        w2ui.toplayout.content('right', w2ui.tldLayout);
-        w2ui.toplayout.show('right', true);
-        w2ui.toplayout.sizeTo('right', 600);
-        w2ui.toplayout.render();
-        app.new_form_rec = false;  // mark as record exists
-        app.form_is_dirty = false; // mark as no changes yet
+    } else {
+        w2ui.tldsDetailGrid.url = '';
+        w2ui.tldsDetailGrid.records = [];
+        w2ui.tldsInfoForm.header = 'Task List Definition (new)';
+        w2ui.tldsInfoForm.record = getTLDInitRecord(bid, null);
     }
+    w2ui.toplayout.content('right', w2ui.tldLayout);
+    w2ui.toplayout.show('right', true);
+    w2ui.toplayout.sizeTo('right', 600);
+    w2ui.toplayout.render();
+    app.new_form_rec = false;  // mark as record exists
+    app.form_is_dirty = false; // mark as no changes yet
 };
 
