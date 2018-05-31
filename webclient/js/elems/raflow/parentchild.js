eb28740c008c96049acafa3735be3557b344d533
Sudip Raval <sudip@auberginesolutions.com>
2018-05-31 15:04:16 +0530

    [raflow]: fix rendering of ParentRentableName (Assing To) column value

--- webclient/js/elems/raflow/parentchild.js
+++ webclient/js/elems/raflow/parentchild.js
@@ -56,8 +56,9 @@ window.loadRAPeopleChildSection = function () {
                         items: [],
                     },
                     render: function (record, index, col_index) {
-                        var html = '';
-                        var items = parentRentableW2UIItems;
+                        var html = '',
+                            items = parentRentableW2UIItems;
+
                         for (var s in items) {
                             if (items[s].id == this.getCellValue(index, col_index)) html = items[s].text;
                         }
@@ -65,17 +66,21 @@ window.loadRAPeopleChildSection = function () {
                     }
                 },
             ],
+            onChange: function(event) {
+                event.onComplete = function() {
+                    this.save();
+                };
+            }
         });
     }
 
     // prepare parent and child rentable list based on rentables section data
     var rentableCompData = getRAFlowCompData("rentables", app.raflow.activeFlowID) || [],
         compData = getRAFlowCompData("parentchild", app.raflow.activeFlowID) || [],
-        tempCompData = [],
-        recidCounter = 0,
+        recidCounter = 1, // always starts with 1
         BID = getCurrentBID(),
         gridRecords = [],
-        parentRentableW2UIItems = [{id: 0, text: " -- select parent rentables -- "}];
+        parentRentableW2UIItems = [];
 
     // always render data from latest modified rentable comp data
     rentableCompData.forEach(function(rentableItem) {
@@ -86,7 +91,7 @@ window.loadRAPeopleChildSection = function () {
             var PRID = 0;
             var cRentable = getChildRentableLocalData(RID);
 
-            // parent Rentable ID
+            // parent Rentable ID found then
             if (cRentable.PRID) {
                 PRID = cRentable.PRID;
             }
@@ -102,16 +107,29 @@ window.loadRAPeopleChildSection = function () {
             };
             recidCounter++;
             gridRecords.push(rec);
-
-            // push the data temporarily
-            tempCompData.push({BID: rec.BID, CRID: rec.CRID, PRID: rec.PRID});
         } else { // 0 = parent rentable
             parentRentableW2UIItems.push({id: rentableItem.RID, text: rentableItem.RentableName});
         }
     });
+    console.debug(parentRentableW2UIItems);
 
-    console.debug(tempCompData);
-    console.debug(compData);
+    // if there is only one parent rentable then pre-select it for all child rentable
+    // otherwise built drop down menu
+    if (parentRentableW2UIItems.length == 0) {
+        gridRecords.forEach(function(rec) {
+            rec.PRID = 0;
+            rec.ParentRentableName = 0;
+        });
+        parentRentableW2UIItems.unshift({id: 0, text: " -- select parent rentables -- "});
+    } else if (parentRentableW2UIItems.length == 1) {
+        // re-assign PRID
+        gridRecords.forEach(function(rec) {
+            rec.PRID = parentRentableW2UIItems[0].id;
+            rec.ParentRentableName = parentRentableW2UIItems[0].id;
+        });
+    } else {
+        parentRentableW2UIItems.unshift({id: 0, text: " -- select parent rentables -- "});
+    }
 
     // now load grid in division
     $('#ra-form #parentchild .grid-container').w2render(w2ui.RAParentChildGrid);
@@ -120,17 +138,16 @@ window.loadRAPeopleChildSection = function () {
     setTimeout(function () {
         var compData = getRAFlowCompData("parentchild", app.raflow.activeFlowID);
         var grid = w2ui.RAParentChildGrid;
-        // assign item prepared earlier for parent rentable list
-        grid.getColumn("ParentRentableName").editable.items = parentRentableW2UIItems;
         grid.records = gridRecords;
         grid.refresh();
 
-        if (compData) {
-            // grid.records = compData;
-            reassignGridRecids(grid.name);
-        } else {
-            grid.clear();
-        }
+        // assign item prepared earlier for parent rentable list
+        grid.getColumn("ParentRentableName").editable.items = parentRentableW2UIItems;
+        grid.getColumn("ParentRentableName").render();
+
+        // if there are any difference between server data and local data at this step
+        // then save the modified data on the server via API
+
     }, 500);
 };
 
