aa429358a362c93a0cc4668a28e05c6706fc5fe4
akshaybosamiya <akshay@auberginesolutions.com>
2018-06-04 15:32:32 +0530

    Update CYPRESS_SPEC

--- test/webclient/functest.sh
+++ test/webclient/functest.sh
@@ -20,8 +20,8 @@ CONFIGPATH="$(pwd)"
 source ../share/base.sh
 
 # specific file that needs to be tested
-#CYPRESS_SPEC="./cypress/integration/*"
-CYPRESS_SPEC="./cypress/integration/roller_12_rentabletypes_spec.js"
+CYPRESS_SPEC="./cypress/integration/*"
+#CYPRESS_SPEC="./cypress/integration/roller_12_rentabletypes_spec.js"
 # ./cypress/integration/roller_2_asms_spec.js
 
 #--------------------------------------------------------------------
