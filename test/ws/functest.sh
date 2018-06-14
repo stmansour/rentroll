5a479f2de0c14558ec796a8cade35799af09f1d6
Sudip Raval <sudip@auberginesolutions.com>
2018-06-14 22:53:02 +0530

    test/ws: FIX the gold files

--- test/ws/functest.sh
+++ test/ws/functest.sh
@@ -34,48 +34,48 @@ dojsonGET "http://localhost:8270/v1/parentaccounts/2" "ws2" "WebService--GetPare
 dojsonGET "http://localhost:8270/v1/postaccounts/2" "ws3" "WebService--GetPostAccountsListForBusiness"
 
 # Get Chart of Accounts
-echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
+echo "request=%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
 dojsonPOST "http://localhost:8270/v1/accounts/2" "request" "ws4"  "WebService--ChartOfAccounts"
 
 # Get Account details
-echo "%7B%22cmd%22%3A%22get%22%2C%22recid%22%3A0%2C%22name%22%3A%22accountForm%22%7D" > request
+echo "request=%7B%22cmd%22%3A%22get%22%2C%22recid%22%3A0%2C%22name%22%3A%22accountForm%22%7D" > request
 dojsonPOST "http://localhost:8270/v1/account/2/97" "request" "ws5" "WebService--ChartOfAccounts-detail"
 
 # Create new Account
-echo "%7B%22cmd%22%3A%22save%22%2C%22recid%22%3A0%2C%22name%22%3A%22%22%2C%22record%22%3A%7B%22LID%22%3A0%2C%22BID%22%3A2%2C%22RAID%22%3A0%2C%22TCID%22%3A0%2C%22GLNumber%22%3A%22123456789%22%2C%22Name%22%3A%22SmokeTest%20GLAccount%22%2C%22AcctType%22%3A%22%22%2C%22Description%22%3A%22%22%2C%22LastModTime%22%3A%221%2F1%2F1900%22%2C%22LastModBy%22%3A0%2C%22BUD%22%3A%22%22%2C%22PLID%22%3A0%2C%22Status%22%3A0%2C%22Type%22%3A0%2C%22AllowPost%22%3Afalse%2C%22ManageToBudget%22%3Afalse%7D%7D" > request
+echo "request%3D%7B%22cmd%22%3A%22save%22%2C%22recid%22%3A0%2C%22name%22%3A%22%22%2C%22record%22%3A%7B%22LID%22%3A0%2C%22BID%22%3A2%2C%22RAID%22%3A0%2C%22TCID%22%3A0%2C%22GLNumber%22%3A%22123456789%22%2C%22Name%22%3A%22SmokeTest%20GLAccount%22%2C%22AcctType%22%3A%22%22%2C%22Description%22%3A%22%22%2C%22LastModTime%22%3A%221%2F1%2F1900%22%2C%22LastModBy%22%3A0%2C%22BUD%22%3A%22%22%2C%22PLID%22%3A0%2C%22Status%22%3A0%2C%22Type%22%3A0%2C%22AllowPost%22%3Afalse%2C%22ManageToBudget%22%3Afalse%7D%7D" > request
 dojsonPOST "http://localhost:8270/v1/account/2/0" "request" "ws6"  "WebService--CreateGLAccount"
 
 # Update Account details
-echo "%7B%22cmd%22%3A%22save%22%2C%22recid%22%3A0%2C%22name%22%3A%22%22%2C%22record%22%3A%7B%22LID%22%3A108%2C%22BID%22%3A2%2C%22RAID%22%3A0%2C%22TCID%22%3A0%2C%22GLNumber%22%3A%229876543210%22%2C%22Name%22%3A%22SmokeTest%20GLAccount%22%2C%22AcctType%22%3A%22%22%2C%22Description%22%3A%22Update%20this%20Account%20(Smoke%20Test)%22%2C%22LastModTime%22%3A%221%2F1%2F1900%22%2C%22LastModBy%22%3A0%2C%22BUD%22%3A%22%22%2C%22PLID%22%3A0%2C%22Status%22%3A0%2C%22Type%22%3A0%2C%22AllowPost%22%3Atrue%2C%22ManageToBudget%22%3Afalse%7D%7D" > request
+echo "request%3D%7B%22cmd%22%3A%22save%22%2C%22recid%22%3A0%2C%22name%22%3A%22%22%2C%22record%22%3A%7B%22LID%22%3A108%2C%22BID%22%3A2%2C%22RAID%22%3A0%2C%22TCID%22%3A0%2C%22GLNumber%22%3A%229876543210%22%2C%22Name%22%3A%22SmokeTest%20GLAccount%22%2C%22AcctType%22%3A%22%22%2C%22Description%22%3A%22Update%20this%20Account%20(Smoke%20Test)%22%2C%22LastModTime%22%3A%221%2F1%2F1900%22%2C%22LastModBy%22%3A0%2C%22BUD%22%3A%22%22%2C%22PLID%22%3A0%2C%22Status%22%3A0%2C%22Type%22%3A0%2C%22AllowPost%22%3Atrue%2C%22ManageToBudget%22%3Afalse%7D%7D" > request
 dojsonPOST "http://localhost:8270/v1/account/2/108" "request" "ws7"  "WebService--UpdateGLAccount"
 
 # Delete Account
-echo "%7B%22cmd%22%3A%22delete%22%2C%22LID%22%3A97%7D" > request
+echo "request%3D%7B%22cmd%22%3A%22delete%22%2C%22LID%22%3A97%7D" > request
 dojsonPOST "http://localhost:8270/v1/account/2/" "request" "ws8"  "WebService--DeleteGLAccount"
 
 # Get Transactants
-echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22sort%22%3A%5B%7B%22field%22%3A%22LastName%22%2C%22direction%22%3A%22asc%22%7D%5D%7D" > request
+echo "request=%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22sort%22%3A%5B%7B%22field%22%3A%22LastName%22%2C%22direction%22%3A%22asc%22%7D%5D%7D" > request
 dojsonPOST "http://localhost:8270/v1/transactants/1" "request" "ws9"  "WebService--GetTransactants"
 
 # Get Rentables
-# echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
-echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22searchDtStart%22%3A%228%2F1%2F2016%22%2C%22searchDtStop%22%3A%229%2F1%2F2016%22%7D" > request
+# echo "request=%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
+echo "request=%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22searchDtStart%22%3A%228%2F1%2F2016%22%2C%22searchDtStop%22%3A%229%2F1%2F2016%22%7D" > request
 dojsonPOST "http://localhost:8270/v1/rentables/1" "request" "ws10"  "WebService--GetRentables"
 
 # Get Receipts
-echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22searchDtStart%22%3A%222016-08-01%22%2C%22searchDtStop%22%3A%222016-09-01%22%7D" > request
+echo "request=%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22searchDtStart%22%3A%222016-08-01%22%2C%22searchDtStop%22%3A%222016-09-01%22%7D" > request
 dojsonPOST "http://localhost:8270/v1/receipts/1" "request" "ws11"  "WebService--GetReceipts"
 
 # Get Assessments
-echo "%7b%22cmd%22%3a%22get%22%2c%22selected%22%3a%5b%5d%2c%22limit%22%3a100%2c%22offset%22%3a0%7d" > request
+echo "request%3d%7b%22cmd%22%3a%22get%22%2c%22selected%22%3a%5b%5d%2c%22limit%22%3a100%2c%22offset%22%3a0%7d" > request
 dojsonPOST "http://localhost:8270/v1/asms/1" "request" "ws12"  "WebService--GetAssessments"
 
 # Get Assessment 1 from REX
-echo "%7B%22cmd%22%3A%22get%22%2C%22recid%22%3A0%2C%22name%22%3A%22asmEpochForm%22%7D" > request
+echo "request=%7B%22cmd%22%3A%22get%22%2C%22recid%22%3A0%2C%22name%22%3A%22asmEpochForm%22%7D" > request
 dojsonPOST "http://localhost:8270/v1/asm/REX/1" "request" "ws13"  "WebService--GetAnAssessment"
 
 # Save the Assessment with an updated comment
-echo "%7B%22cmd%22%3A%22save%22%2C%22recid%22%3A0%2C%22name%22%3A%22asmInstForm%22%2C%22record%22%3A%7B%22recid%22%3A0%2C%22ASMID%22%3A43%2C%22BID%22%3A1%2C%22BUD%22%3A%22REX%22%2C%22PASMID%22%3A14%2C%22RID%22%3A1%2C%22Rentable%22%3A%22309%20Rexford%22%2C%22RAID%22%3A1%2C%22Amount%22%3A3750%2C%22Start%22%3A%2212%2F1%2F2016%22%2C%22Stop%22%3A%2212%2F2%2F2016%22%2C%22RentCycle%22%3A6%2C%22ProrationCycle%22%3A4%2C%22InvoiceNo%22%3A0%2C%22ARID%22%3A0%2C%22Comment%22%3A%22comment%20by%20sman%22%2C%22LastModTime%22%3A%226%2F6%2F2017%22%2C%22LastModBy%22%3A0%7D%7D" > request
+echo "request=%7B%22cmd%22%3A%22save%22%2C%22recid%22%3A0%2C%22name%22%3A%22asmInstForm%22%2C%22record%22%3A%7B%22recid%22%3A0%2C%22ASMID%22%3A43%2C%22BID%22%3A1%2C%22BUD%22%3A%22REX%22%2C%22PASMID%22%3A14%2C%22RID%22%3A1%2C%22Rentable%22%3A%22309%20Rexford%22%2C%22RAID%22%3A1%2C%22Amount%22%3A3750%2C%22Start%22%3A%2212%2F1%2F2016%22%2C%22Stop%22%3A%2212%2F2%2F2016%22%2C%22RentCycle%22%3A6%2C%22ProrationCycle%22%3A4%2C%22InvoiceNo%22%3A0%2C%22ARID%22%3A0%2C%22Comment%22%3A%22comment%20by%20sman%22%2C%22LastModTime%22%3A%226%2F6%2F2017%22%2C%22LastModBy%22%3A0%7D%7D" > request
 dojsonPOST "http://localhost:8270/v1/asm/REX/1" "request" "ws14"  "WebService--SaveAnAssessment"
 
 # Get Receipt 5 from REX
@@ -97,11 +97,11 @@ echo "%7B%22cmd%22%3A%22save%22%2C%22recid%22%3A0%2C%22name%22%3A%22receiptForm%
 dojsonPOST "http://localhost:8270/v1/receipt/REX/0" "request" "ws18"  "WebService--InsertAReceipt-Fai"
 
 # Create a NEW ASSESSMENT
-echo "%7B%22cmd%22%3A%22save%22%2C%22recid%22%3A0%2C%22name%22%3A%22asmEpochForm%22%2C%22record%22%3A%7B%22ARID%22%3A2%2C%22recid%22%3A0%2C%22RID%22%3A1%2C%22ASMID%22%3A0%2C%22PASMID%22%3A0%2C%22ATypeLID%22%3A0%2C%22InvoiceNo%22%3A0%2C%22RAID%22%3A1%2C%22BID%22%3A1%2C%22BUD%22%3A%22REX%22%2C%22Start%22%3A%226%2F6%2F2017%22%2C%22Stop%22%3A%226%2F6%2F2017%22%2C%22RentCycle%22%3A6%2C%22ProrationCycle%22%3A4%2C%22TCID%22%3A0%2C%22Amount%22%3A60%2C%22AcctRule%22%3A%22%22%2C%22Comment%22%3A%22%22%2C%22LastModTime%22%3A%226%2F6%2F2017%22%2C%22LastModBy%22%3A0%2C%22Rentable%22%3A%22309%2BRexford%22%7D%7D" > request
+echo "request=%7B%22cmd%22%3A%22save%22%2C%22recid%22%3A0%2C%22name%22%3A%22asmEpochForm%22%2C%22record%22%3A%7B%22ARID%22%3A2%2C%22recid%22%3A0%2C%22RID%22%3A1%2C%22ASMID%22%3A0%2C%22PASMID%22%3A0%2C%22ATypeLID%22%3A0%2C%22InvoiceNo%22%3A0%2C%22RAID%22%3A1%2C%22BID%22%3A1%2C%22BUD%22%3A%22REX%22%2C%22Start%22%3A%226%2F6%2F2017%22%2C%22Stop%22%3A%226%2F6%2F2017%22%2C%22RentCycle%22%3A6%2C%22ProrationCycle%22%3A4%2C%22TCID%22%3A0%2C%22Amount%22%3A60%2C%22AcctRule%22%3A%22%22%2C%22Comment%22%3A%22%22%2C%22LastModTime%22%3A%226%2F6%2F2017%22%2C%22LastModBy%22%3A0%2C%22Rentable%22%3A%22309%2BRexford%22%7D%7D" > request
 dojsonPOST "http://localhost:8270/v1/asm/1/0" "request" "ws19"  "WebService--InsertAnAssessment"
 
 # Test Transactant Typedown
-dojsonGET "http://localhost:8270/v1/transactantstd/ISO?%7B%22search%22%3A%22s%22%2C%22max%22%3A250%7D" "ws20" "WebService--GetTransactantTypeDown"
+dojsonGET "http://localhost:8270/v1/transactantstd/ISO?request=%7B%22search%22%3A%22s%22%2C%22max%22%3A250%7D" "ws20" "WebService--GetTransactantTypeDown"
 
 # Create a NEW Rentable User
 echo "%7B%22cmd%22%3A%22save%22%2C%22formname%22%3A%22tcidPicker%22%2C%22record%22%3A%7B%22recid%22%3A1%2C%22BID%22%3A5%2C%22TCID%22%3A373%2C%22RID%22%3A16%2C%22FirstName%22%3A%22Jason%22%2C%22MiddleName%22%3A%22%22%2C%22LastName%22%3A%22Thomas%22%2C%22DtStart%22%3A%223%2F5%2F2017%22%2C%22DtStop%22%3A%223%2F5%2F2018%22%7D%7D" > request
@@ -112,7 +112,7 @@ echo "%7B%22cmd%22%3A%22save%22%2C%22formname%22%3A%22tcidPicker%22%2C%22record%
 dojsonPOST "http://localhost:8270/v1/ruser/ISO/16" "request" "ws22"  "WebService--InsertARentableUser"
 
 # Delete a Rentable User
-echo "%7B%22cmd%22%3A%22delete%22%2C%22selected%22%3A%5B1%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22TCID%22%3A373%7D" > request
+echo "request%3D%7B%22cmd%22%3A%22delete%22%2C%22selected%22%3A%5B1%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22TCID%22%3A373%7D" > request
 dojsonPOST "http://localhost:8270/v1/ruser/ISO/16" "request" "ws23"  "WebService--DeleteARentableUser"
 
 # Create another NEW RAID Payor -- same TCID
@@ -120,42 +120,42 @@ echo "%7B%22cmd%22%3A%22save%22%2C%22formname%22%3A%22tcidPicker%22%2C%22record%
 dojsonPOST "http://localhost:8270/v1/rapayor/ISO/16" "request" "ws24"  "WebService--InsertARAIDPayor"
 
 # Read RAID Payors
-echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
+echo "request=%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
 dojsonPOST "http://localhost:8270/v1/rapayor/ISO/16" "request" "ws25"  "WebService--GetRAIDPayors"
 
 # # Delete a RAID Payor that does not exist for the the specified RAID (it should go to RAID 20 but will go to RAID 16 instead)
-# echo "%7B%22cmd%22%3A%22delete%22%2C%22selected%22%3A%5B1049%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22TCID%22%3A367%2C%22DtStop%22%3A%223%2F11%2F2018%22%7D" > request
+# echo "request=%7B%22cmd%22%3A%22delete%22%2C%22selected%22%3A%5B1049%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22TCID%22%3A367%2C%22DtStop%22%3A%223%2F11%2F2018%22%7D" > request
 # dojsonPOST "http://localhost:8270/v1/rapayor/ISO/16" "request" "ws26"  "WebService--DeleteARentablePayor-forceError"
 
 # Delete a RAID Payor that does not exist for the the specified RAID
-# echo "%7B%22cmd%22%3A%22delete%22%2C%22selected%22%3A%5B1%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22TCID%22%3A367%7D" > request
+# echo "request%3D%7B%22cmd%22%3A%22delete%22%2C%22selected%22%3A%5B1%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22TCID%22%3A367%7D" > request
 # dojsonPOST "http://localhost:8270/v1/rapayor/ISO/16" "request" "ws27"  "WebService--DeleteARentablePayor"
 
 # # Read RAID Payors
-# echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
+# echo "request=%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
 # dojsonPOST "http://localhost:8270/v1/rapayor/ISO/16" "request" "ws28"  "WebService--GetRAIDPayors"
 
 # Read RAID Users
-echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22TCID%22%3A52%7D" > request
+echo "request%3D%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22TCID%22%3A52%7D" > request
 dojsonPOST "http://localhost:8270/v1/ruser/CCC/10" "request" "ws29"  "WebService--GetRAIDPayors"
 
 # Test Transactant Typedown
-dojsonGET "http://localhost:8270/v1/rentablestd/ISO?%7B%22search%22%3A%226%22%2C%22max%22%3A250%7D" "ws30" "WebService--GetRentableTypeDown"
+dojsonGET "http://localhost:8270/v1/rentablestd/ISO?request%3D%7B%22search%22%3A%226%22%2C%22max%22%3A250%7D" "ws30" "WebService--GetRentableTypeDown"
 
 # Search Payment Types
-echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
+echo "request%3D%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
 dojsonPOST "http://localhost:8270/v1/pmts/1" "request" "ws31"  "WebService--PaymentTypes-SearchAll"
 
 # Get Specificy PaymentType - FORCE ERROR - no PMTID provided
-echo "%7B%22cmd%22%3A%22get%22%2C%22recid%22%3A0%2C%22name%22%3A%22paymentTypeGrid%22%7D" > request
+echo "request%3D%7B%22cmd%22%3A%22get%22%2C%22recid%22%3A0%2C%22name%22%3A%22paymentTypeGrid%22%7D" > request
 dojsonPOST "http://localhost:8270/v1/pmts/1" "request" "ws32"  "WebService--PaymentTypes-Get-ForceError"
 
 # Get Specificy PaymentType
-echo "%7B%22cmd%22%3A%22get%22%2C%22recid%22%3A0%2C%22name%22%3A%22paymentTypeGrid%22%7D" > request
+echo "request%3D%7B%22cmd%22%3A%22get%22%2C%22recid%22%3A0%2C%22name%22%3A%22paymentTypeGrid%22%7D" > request
 dojsonPOST "http://localhost:8270/v1/pmts/1/1" "request" "ws33"  "WebService--PaymentTypes-Get"
 
 # Get Specificy PaymentType
-# echo "%7B%22cmd%22%3A%22get%22%2C%22recid%22%3A0%2C%22name%22%3A%22paymentTypeGrid%22%7D" > request
+# echo "request%3D%7B%22cmd%22%3A%22get%22%2C%22recid%22%3A0%2C%22name%22%3A%22paymentTypeGrid%22%7D" > request
 # dojsonPOST "http://localhost:8270/v1/pmt/1/1" "request" "ws34"  "WebService--PaymentTypes"
 
 # get UI Values...
@@ -163,13 +163,13 @@ doPlainGET "http://localhost:8270/v1/uival/REX/app.AssessmentRules" "ws35" "WebS
 doPlainGET "http://localhost:8270/v1/uival/REX/app.ReceiptRules" "ws36" "WebService--GetUIValue-app.ReceiptRules"
 
 # rental Agreement typedown...
-dojsonGET "http://localhost:8270/v1/rentalagrtd/CCC?%7B%22search%22%3A%22s%22%2C%22max%22%3A250%7D" "ws37" "WebService--GetRentalAgreementTypeDown"
+dojsonGET "http://localhost:8270/v1/rentalagrtd/CCC?request=%7B%22search%22%3A%22s%22%2C%22max%22%3A250%7D" "ws37" "WebService--GetRentalAgreementTypeDown"
 
 # get Rentable types list for a business
 dojsonGET "http://localhost:8270/v1/rtlist/2" "ws38" "WebService--GetRentableTypesForBusiness"
 
 # save rentable
-echo "%7B%22cmd%22%3A%22save%22%2C%22recid%22%3A0%2C%22name%22%3A%22rentableForm%22%2C%22record%22%3A%7B%22recid%22%3A0%2C%22BID%22%3A1%2C%22BUD%22%3A%22REX%22%2C%22RID%22%3A0%2C%22RentableName%22%3A%22REX-Test-1%22%2C%22AssignmentTime%22%3A1%7D%7D" > request
+echo "request=%7B%22cmd%22%3A%22save%22%2C%22recid%22%3A0%2C%22name%22%3A%22rentableForm%22%2C%22record%22%3A%7B%22recid%22%3A0%2C%22BID%22%3A1%2C%22BUD%22%3A%22REX%22%2C%22RID%22%3A0%2C%22RentableName%22%3A%22REX-Test-1%22%2C%22AssignmentTime%22%3A1%7D%7D" > request
 dojsonPOST "http://localhost:8270/v1/rentable/1/0" "request" "ws39"  "WebService--SaveRentable"
 
 # save rentable status with usestatus: 2(Administrative), leaseStatus: 5(Leased)
@@ -181,16 +181,16 @@ echo "%7B%22cmd%22%3A%22save%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C
 dojsonPOST "http://localhost:8270/v1/rentabletyperef/1/1129" "request" "ws41"  "WebService--SaveRentableTypeRef-Rentable(1129)"
 
 # Get Rentables
-# echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
-echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22searchDtStart%22%3A%228%2F1%2F2016%22%2C%22searchDtStop%22%3A%229%2F1%2F2016%22%7D" > request
+# echo "request=%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
+echo "request=%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%2C%22searchDtStart%22%3A%228%2F1%2F2016%22%2C%22searchDtStop%22%3A%229%2F1%2F2016%22%7D" > request
 dojsonPOST "http://localhost:8270/v1/rentables/1" "request" "ws42"  "WebService--GetRentables"
 
 # Get Rentable Status list for Rentable(1129)
-echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
+echo "request=%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
 dojsonPOST "http://localhost:8270/v1/rentablestatus/1/1129" "request" "ws43"  "WebService--GetRentableStatus"
 
 # Get Rentable Type ref list for Rentable(1129)
-echo "%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
+echo "request=%7B%22cmd%22%3A%22get%22%2C%22selected%22%3A%5B%5D%2C%22limit%22%3A100%2C%22offset%22%3A0%7D" > request
 dojsonPOST "http://localhost:8270/v1/rentabletyperef/1/1129" "request" "ws44"  "WebService--GetRentableTypeRef"
 
 # delete rentable status recently created (1129)
