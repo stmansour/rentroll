2543bed648e9a588556746b6abb2cdd48eb0020b
Sudip Raval <sudip@auberginesolutions.com>
2018-06-27 01:36:58 +0530

    rlib/bizprops: ADD missing go file with listing down pet, vehicle fees

---
+++ rlib/bizprops.go
@@ -0,0 +1,115 @@
+package rlib
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+// BizPropsVehicleFee struct holds detailed info about vehicle fee
+// configured in business properties
+type BizPropsVehicleFee struct {
+	BID    int64
+	ARID   int64
+	ARName string
+	Amount float64
+}
+
+// BizPropsPetFee struct holds detailed info about pet fee
+// configured in business properties
+type BizPropsPetFee struct {
+	BID    int64
+	ARID   int64
+	ARName string
+	Amount float64
+}
+
+// GetPetFeesFromGeneralBizProps returns pet fees with detailed data
+// defined in BizPropsPetFee
+func GetPetFeesFromGeneralBizProps(ctx context.Context, BID int64) (fees []BizPropsPetFee, err error) {
+	const funcname = "GetPetFeesFromGeneralBizProps"
+	var (
+		bizPropName = "general"
+		bizPropJSON = BizProps{}
+	)
+	fmt.Printf("Entered in %s\n", funcname)
+
+	// get business properties
+	var bizProp BusinessProperties
+	bizProp, err = GetBusinessPropertiesByName(ctx, bizPropName, BID)
+	if err != nil {
+		return
+	}
+
+	// get json doc from Data column
+	if err = json.Unmarshal(bizProp.Data, &bizPropJSON); err != nil {
+		return
+	}
+
+	// get pet Fees
+	for _, n := range bizPropJSON.PetFees {
+		var pf BizPropsPetFee
+		var ar AR
+		ar, err = GetARByName(ctx, BID, n)
+		if err != nil {
+			return
+		}
+
+		// migrate values from ar to pf
+		pf.BID = ar.BID
+		pf.ARID = ar.ARID
+		pf.ARName = ar.Name
+		pf.Amount = ar.DefaultAmount
+
+		// append in the list
+		fees = append(fees, pf)
+	}
+
+	// return finally
+	return
+}
+
+// GetVehicleFeesFromGeneralBizProps returns vehicle fees with detailed data
+// defined in BizPropsVehicleFee
+func GetVehicleFeesFromGeneralBizProps(ctx context.Context, BID int64) (fees []BizPropsVehicleFee, err error) {
+	const funcname = "GetVehicleFeesFromGeneralBizProps"
+	var (
+		bizPropName = "general"
+		bizPropJSON = BizProps{}
+	)
+	fmt.Printf("Entered in %s\n", funcname)
+
+	// get business properties
+	var bizProp BusinessProperties
+	bizProp, err = GetBusinessPropertiesByName(ctx, bizPropName, BID)
+	if err != nil {
+		return
+	}
+
+	// get json doc from Data column
+	if err = json.Unmarshal(bizProp.Data, &bizPropJSON); err != nil {
+		return
+	}
+
+	// get pet Fees
+	for _, n := range bizPropJSON.VehicleFees {
+		var vf BizPropsVehicleFee
+		var ar AR
+		ar, err = GetARByName(ctx, BID, n)
+		if err != nil {
+			return
+		}
+
+		// migrate values from ar to vf
+		vf.BID = ar.BID
+		vf.ARID = ar.ARID
+		vf.ARName = ar.Name
+		vf.Amount = ar.DefaultAmount
+
+		// append in the list
+		fees = append(fees, vf)
+	}
+
+	// return finally
+	return
+}
